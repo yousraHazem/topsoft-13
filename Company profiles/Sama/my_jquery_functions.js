19a035986d56ad572973ddd12885128fddba1f4d
sama akram <samaakram92@gmail.com>
2013-02-22 20:31:34 +0200

    added sama

---
+++ Company profiles/Sama/my_jquery_functions.js
@@ -0,0 +1,20 @@
+ $(document).ready(function(){
+		$("#b1").click(function(){
+                $("#b1off").toggle();
+		});
+		});
+ $(document).ready(function(){
+		$("#b2").click(function(){
+                $("#b2off").slidetoggle("slow");
+		});
+		
+$(document).ready(function(){ 
+						   
+	$("div").css("border", "3px solid red");
+	
+});
+$(document).ready(function(){
+  $("b1").click(function(){
+    $("#b1off").show();
+  });
+});
