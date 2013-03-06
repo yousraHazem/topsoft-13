b583ddb6e51f9795b6124056d0d91584c4e69484
Donia Amer Shaarawy <shaarawy.donia@gmail.com>
2013-03-06 13:48:45 +0200

    ddd

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303113655) do
+ActiveRecord::Schema.define(:version => 20130304162612) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(:version => 20130303113655) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
