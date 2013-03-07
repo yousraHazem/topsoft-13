854aab9dad7c832fe856280d2a6dc846e3f21392
Riham Gamal <riham.gamal93@gmail.com>
2013-03-07 16:54:55 +0200

    Added my work

--- django/editor/models.py
+++ django/editor/models.py
@@ -25,3 +25,9 @@ class Users(models.Model):
     userPassword = models.CharField(max_length = 10)
     def __unicode__(self):
         return self.userName
+
+class Admin(models.Model):
+    adminName = models.CharField(max_length = 200)
+    adminPassword = models.CharField(max_length = 10)
+    def __unicode__(self):
+        return self.adminName
