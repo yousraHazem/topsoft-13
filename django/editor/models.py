2a86a4008a4b4ad8cc854b5e91a40d8bc4096d50
nayera-muhammad <nayera.muhammad@gmail.com>
2013-03-07 20:20:56 +0200

    pulling

---
+++ django/editor/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Editor(models.Model):
+    editorName = models.CharField(max_length = 200)
+    editorPassword = models.CharField(max_length = 10)
+    def __unicode__(self):
+        return self.editorName
+
+class Post(models.Model):
+    theeditor = models.ForeignKey(Editor)
+    text = models.CharField(max_length = 1000)
+    def __unicode__(self):
+        return self.text
+
+class Comment(models.Model):
+    thepost = models.ForeignKey(Post)
+    content = models.CharField(max_length = 1000)
+    def __unicode__(self):
+        return self.content
+
+class Users(models.Model):
+    theComment = models.ForeignKey(Comment)
+    userName = models.CharField(max_length = 200)
+    userPassword = models.CharField(max_length = 10)
+    def __unicode__(self):
+        return self.userName
