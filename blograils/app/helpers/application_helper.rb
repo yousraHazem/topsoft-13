80ed179a9fcf69bfca1b989dc6c0c3c532dac400
sama akram abdelrahman <samaakram92@gmail.com>
2013-03-08 16:30:58 +0200

    addla

--- blograils/app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApplicationHelper
-	def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
-		puts "HEREEEEEE #{@current_user}"
-	end
-end
--- blograils/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '639e17978b33dde5e149b37e37e1faab60c4f6b3b5304a8e639fa56ef639f28587e9904387b77f3e6468e8ef4de6c6355cd1c86403bdbc2981a599e79accd46c'
