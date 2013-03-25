3c3468760ece6b5132f7e391ed83619cd25a7d1c
Donia Amer Shaarawy <shaarawy.donia@gmail.com>
2013-03-25 18:24:19 +0200

    table changes

--- MME7/app/models/project.rb
+++ MME7/app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ActiveRecord::Base
 
 	has_many :posts
-	has_many :users #project creators 
 	has_many :tasks 
 	has_and_belongs_to_many :users
 	has_one :budget 
--- MME7/app/models/user.rb
+++ MME7/app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   has_many :comments
   belongs_to :task
   belongs_to :group
-  has_and_belongs_to_many :groups 
-  has_and_belongs_to_many :projects
   has_and_belongs_to_many :budget_items
   has_many :project_users
   has_many :projects , :through => :project_users
+   has_many :groups_users 
+   has_many :groups , :through => :group_users 
 end
