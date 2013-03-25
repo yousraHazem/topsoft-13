5b54a93aca1dbf16b758f8815fcd092c7ed598e6
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-03-25 19:15:17 +0200

    update

--- MME7/app/models/user.rb
+++ MME7/app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  #attr_accessible :title, :body
   has_many :posts
   has_many :comments
   belongs_to :task
   belongs_to :group
   has_and_belongs_to_many :groups 
-  has_and_belongs_to_many :projects
   has_and_belongs_to_many :budget_items
   has_many :project_users
   has_many :projects , :through => :project_users
