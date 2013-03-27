b5d7852a4cf0695cac14c99fabd61988210c4d65
May Atef Badr <may.badr1991@gmail.com>
2013-03-28 01:20:38 +0200

    Added post method

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -1,2 +1,5 @@
 class CommentsController < ApplicationController
+
+	
+
 end
--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+	def newPost
+		@post = Post.new
+	end
+
+	def createPost
+		@post = Post.new(params[:post])
+		if @post.save
+			flash[:notice] = "Post successfully created"
+			#redirect_to("")
+		else
+			flash[:notice] = "Post could not be created"
+			render('newPost')
+		end
+	end
+
+	def editPost
+		@post = Post.find(param[:id])
+	end
+	
+	def updatePost
+		@post = Post.find(param[:id])
+		if @post.update_attributes(param[:post])
+			flash[:notice] = "Post successfully updated"
+			render("updatePost")
+		else
+			flash[:notice] = "Post could not be updated"
+			render("editPost")
+		end
+	end
+
+
 end
