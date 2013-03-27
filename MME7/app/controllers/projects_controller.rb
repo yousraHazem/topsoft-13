96a7bf125b1230591bcae95a3f3ee554ee715ed8
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-03-27 23:46:19 +0200

    added my edit and get projects methods

--- MME7/app/controllers/projects_controller.rb
+++ MME7/app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+  
+	
+
+    #gets the project that need to be edited
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+    #update the selected project with the new submitted values
+	def update
+			@project = Project.find(params[:id])
+		if @project.update_attributes(params[:project])
+			flash[:notice] = "Project successfully updated"
+			#redirect_to(:action=>'ed', :id=>)
+			render('edit')
+		else
+		   flash[:notice] = "Project could not be updated"
+          # render('edit')
+		end
+	end
 end
--- MME7/app/helpers/projects_helper.rb
+++ MME7/app/helpers/projects_helper.rb
@@ -1,2 +1,7 @@
 module ProjectsHelper
+
+  #returns all the projects in the table
+	def get_all_projects
+		return Project.order("projects.id ASC")
+	end
 end
