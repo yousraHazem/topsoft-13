721fa791dae485ff597f4f525fd2388613670774
nayera-muhammad <nayera.muhammad@gmail.com>
2013-03-28 00:20:39 +0200

    added file for getting project members and applied modifications on user.rb and project_user.rb and added my UMLs

--- MME7/app/helpers/project_users_helper.rb
+++ MME7/app/helpers/project_users_helper.rb
@@ -1,2 +1,7 @@
 module ProjectUsersHelper
+
+    #getting the project members
+	def get_projectmembers(project_id)
+ 	     @projectmembersid = ProjectUser.find(:all, :conditions => {:project_id => project_id })
+    end
 end
--- MME7/app/models/project_user.rb
+++ MME7/app/models/project_user.rb
@@ -1,5 +1,5 @@
 class ProjectUser < ActiveRecord::Base
-   attr_accessible :project_id , :user_id , :is_creator
+  attr_accessible :project_id , :user_id , :is_creator
   belongs_to :project
   belongs_to :user
 end
