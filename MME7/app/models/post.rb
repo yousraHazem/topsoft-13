2b5167be635076027411ffcd2c675fe092d55b3d
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-03-27 22:43:08 +0200

    getposts and get_tasks

--- MME7/app/models/post.rb
+++ MME7/app/models/post.rb
@@ -6,4 +6,11 @@ class Post < ActiveRecord::Base
 	belongs_to :project
 	belongs_to :group 
 
+	def getposts (int g_id)
+	#this method takes input from the user the id of their group and find 
+	#the post that has this group id 
+	#then returns the posts with that specified id  
+	#by Toka Omar
+    @posts = Post.find(:all, :conditions => { :group_id => g_id })
+    end
 end
