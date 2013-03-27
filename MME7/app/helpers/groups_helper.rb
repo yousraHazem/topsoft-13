23f06896f3bcd6ac756bf51a22c50d6f770876d5
Donia Amer Shaarawy <shaarawy.donia@gmail.com>
2013-03-27 21:09:47 +0200

    isCreator

--- MME7/app/helpers/groups_helper.rb
+++ MME7/app/helpers/groups_helper.rb
@@ -1,2 +1,16 @@
 module GroupsHelper
+		def isGroupCreator( user_id)
+	groupCreator = Group_Users.where(:group_id => this.id, :user_id => user_id, :is_creator => true).exists?
+	if groupCreator == true
+		return true
+	else 
+		return false 
+	end 
+	end 
+
+
+#this is a method that shows if the current user the person 
+#who is loged in is the creator of the group or not.  
+
+
 end
--- MME7/app/models/group.rb
+++ MME7/app/models/group.rb
@@ -1,7 +1,12 @@
 class Group < ActiveRecord::Base
-   attr_accessible :group_name , :description , :levels
+  attr_accessible :group_name , :description , :levels
+
   has_many :posts 
   has_and_belongs_to_many :users
   has_many :groups_users 
   has_many :users , :through => :group_users
-end
+
+
+
+
+end 
