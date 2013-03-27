8c0b1e132769cf5f98b36ea33e0da6a103542a89
salmaelruby <salmaelruby@gmail.com>
2013-03-28 01:54:35 +0200

    added method delete post

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
-end
+# As a system I can delete a post
++    def destroy
++
++        @p = Post.find(params[:id])
++        if isCreator(current_user.id) == 'true'
++            @p.destroy
++            flash[:notice] = "post successfully deleted!"
++       
++        else
++            flash[:error] = "not allowed"
++          
++    end
+ end
+
