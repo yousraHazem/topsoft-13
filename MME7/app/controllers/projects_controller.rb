2fc2bf086c9a6f60c9c29fa50ab82827d9ca753e
nayera-muhammad <nayera.muhammad@gmail.com>
2013-03-28 00:06:26 +0200

    added files for creating a project including its views

--- MME7/app/controllers/projects_controller.rb
+++ MME7/app/controllers/projects_controller.rb
@@ -1,2 +1,24 @@
 class ProjectsController < ApplicationController
+
+#listing the projects 
+def list
+	@projects = Project.all
+end
+
+def show
+	@project = Project.find(params[:id])
+end
+
+#a new project is created
+def new
+	@project = Project.new
+end
+
+def create
+  @project=Project.new(params[:project])
+  @project.save
+  flash[:notice]= "project created"
+end
+
+	
 end
