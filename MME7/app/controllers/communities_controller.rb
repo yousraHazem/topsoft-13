7ba9af6fd8a2981fa34df43c41659b13de23a55b
May Atef Badr <may.badr1991@gmail.com>
2013-03-28 00:57:44 +0200

    Added delete community method

--- MME7/app/controllers/communities_controller.rb
+++ MME7/app/controllers/communities_controller.rb
@@ -1,2 +1,12 @@
 class CommunitiesController < ApplicationController
+
+	def delete 
+		@community = Community.find(params[:id])
+	end
+
+	def destroy 
+		Community.find(params[:id].destroy
+		redirect_to (:action =>'application')
+	end
+	
 end
