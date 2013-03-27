92af3dffd0973f20e7a89abba3bbcb451a35f4a2
nayera-muhammad <nayera.muhammad@gmail.com>
2013-03-28 00:13:16 +0200

    added file for isCommentCreator and applied some modifications in the comment.rb and post.rb

--- MME7/app/helpers/comments_helper.rb
+++ MME7/app/helpers/comments_helper.rb
@@ -1,2 +1,13 @@
 module CommentsHelper
+
+    #seeing whether if the this is the comment creator or not
+	def isCommentCreator(user_id, comment_id)
+	commentCreator = Comment.where(:comment_id => comment_id, :user_id => user_id, :is_creator => true).exists?
+	if commentCreator == true
+		return true
+	else 
+		return false 
+	end 
+	end 
+
 end
