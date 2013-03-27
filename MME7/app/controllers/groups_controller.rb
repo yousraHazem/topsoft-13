122afd5c94b233eadefa91fa0aac0161e2408993
May Atef Badr <may.badr1991@gmail.com>
2013-03-27 23:49:57 +0200

    Added edit group method

--- MME7/app/controllers/groups_controller.rb
+++ MME7/app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+	def edit 
+		@group = Group.find(params[:id])
+	end
+
+	def update
+		@group = Group.find(params[:id])
+		if @group.update_attributes(params[:group_name])
+			redirect_to (:action =>'show', :id => @group.id)
+		else if  @group.update_attributes(params[:description])
+			redirect_to (:action =>'show', :id => @group.id)
+		else if @group.update_attributes(params[:levels])
+			redirect_to (:action =>'show', :id => @group.id)
+		else 
+			render('edit')
+		end 
+
+	end
 end
