c0086a414a91bf267cd7062e1b2fef2357920acc
mariam <mariamismael@live.com>
2013-03-28 00:46:30 +0200

    Added the create community method

--- MME7/app/controllers/communities_controller.rb
+++ MME7/app/controllers/communities_controller.rb
@@ -1,2 +1,29 @@
 class CommunitiesController < ApplicationController
+
+
+	def list
+
+		@communities = Community.order("communities.title ASC")
+		
+	end
+
+	def show
+		@communities= Community.find(params[:id])
+		
+	end
+def new
+
+	@community= Community.new
+	
+end
+
+def create
+	@community= Community.new(params[:community])
+	   @community.save
+	render ('create')
+
+
+
+end
+
 end
