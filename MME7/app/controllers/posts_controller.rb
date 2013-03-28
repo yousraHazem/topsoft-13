184d05b7a7288885dc50d9e3bbc6705c3fe2e3b6
Riham Gamal <riham.gamal93@gmail.com>
2013-03-28 21:28:25 +0200

    create new post

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
-
+	# add a new post 
 	def newPost
 		@post = Post.new
 	end
 
-
+	# create a new post
 	def createPost
 		@post = Post.new(params[:post])
 
+		# if the post is saved, 
 		if @post.save
 			flash[:notice] = "Post successfully created"
 			redirect_to(:action => 'newPost')
+		# if the post is not saved, 
 		else
 			flash[:notice] = "Post could not be created"
 			render('newPost')
