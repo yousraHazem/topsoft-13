ec3c1327c95ee69369983c6e68961ee24b4f661c
nayera-muhammad <nayera.muhammad@gmail.com>
2013-03-28 22:39:02 +0200

    applied changes to doc

--- MME7/app/helpers/comments_helper.rb
+++ MME7/app/helpers/comments_helper.rb
@@ -1,5 +1,6 @@
 module CommentsHelper
 
+<<<<<<< Updated upstream
     #seeing whether if the this is the comment creator or not
 	def isCommentCreator(user_id, comment_id)
 	commentCreator = Comment.where(:comment_id => comment_id, :user_id => user_id, :is_creator => true).exists?
@@ -9,5 +10,13 @@ module CommentsHelper
 		return false 
 	end 
 	end 
+=======
+    #Author: Nayera Mohamed 22-3789 , this method sees whether if the this is the comment creator or not
+	
+	def isCommentCreator(current_user, comment_id)
+        Comment.where(:user_id => current_user, :comment_id => comment_id, :is_creator => true).exists?  
+    end 
+ 
+>>>>>>> Stashed changes
 
 end
