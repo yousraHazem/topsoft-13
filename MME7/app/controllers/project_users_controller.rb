cc1c11a37e4fd5bb75efa974b1f0a5039c640c07
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-03-28 18:44:01 +0200

    done

--- MME7/app/controllers/project_users_controller.rb
+++ MME7/app/controllers/project_users_controller.rb
@@ -1,19 +1,2 @@
 class ProjectUsersController < ApplicationController
-	
-	#this method checks if the current user is a creator of the project he is trying to access 
-	def isCreator 
-		@creator = project_users.find(:all, :conditions=> {:user_id => current_user.id, :project_id => params[:id]})
-		if @creator => nil
-			return false
-		else
-			return true
-			
-		end
-
-	end 
-
-	#This 
-	def addMember
-
-	end
 end
