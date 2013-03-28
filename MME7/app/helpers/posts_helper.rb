65e289e19a7fe302ed0e254687da35129232cbf9
salmaelruby <salmaelruby@gmail.com>
2013-03-28 02:10:44 +0200

    added method iscreator

--- MME7/app/helpers/posts_helper.rb
+++ MME7/app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+	#method to check if the user is the owner of this post
+	def isCreator (current_user, post_id)
+		Post.where(:user_id => current_user, :post_id => post_id ).exists?
 end
