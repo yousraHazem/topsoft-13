e7e5e1c0cfe3d11ecb0468971014e6f643318c04
Riham Gamal <riham.gamal93@gmail.com>
2013-03-28 22:11:43 +0200

    Edit a comment

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+	# fimd the comment you want to edit
+	def editComment
+		@comment = Comment.find(params[:id])
+	end
+
+	# update the comment by finding its id and chnaging the fields
+	def updateComment
+		@comment = Comment.find(params[:id])
+		rescue_from ActiveRecord::RecordNotFound do
+  		flash[:notice] = 'The object you tried to access does not exist'
+  		render :not_found   # or e.g. redirect_to :action => :index
+		end
+
+		if @comment.update_attributes(param[:comment])
+			flash[:notice] = "Comment successfully updated"
+			render("updateComment")
+		else
+			flash[:notice] = "Comment could not be updated"
+			render("editComment")
+		end
+	end
 end
+
+
