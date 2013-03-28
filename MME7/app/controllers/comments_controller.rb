07ec48def550965ef455bbacc1c6d68e2ebb60e9
Riham Gamal <riham.gamal93@gmail.com>
2013-03-28 21:34:58 +0200

    create new comment

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -1,10 +1,22 @@
 class CommentsController < ApplicationController
 	
+	# initiate comment
 	def newComment
 		@comment = Comment.new
 	end
 
+	# create a new comment
+	def createComment
+		@comment = Comment.new(params[:comment])
 
-	
+		if @comment.save
+			flash[:notice] = "Comment successfully created"
+			redirect_to(:action => 'newComment')
+		else
+			flash[:notice] = "Comment could not be created"
+			render('newComment')
+		end
+
+	end
 
 end
