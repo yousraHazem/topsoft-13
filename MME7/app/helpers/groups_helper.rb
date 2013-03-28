7babf487786e5587c3c8f4ca573dad26bf02bd65
sama akram abdelrahman <samaakram92@gmail.com>
2013-03-28 21:30:56 +0200

    added delete group

--- MME7/app/helpers/groups_helper.rb
+++ MME7/app/helpers/groups_helper.rb
@@ -1,15 +1,2 @@
 module GroupsHelper
-
-    # As a system I can delete a group
-    def destroy
-
-        @group = Groups.find(params[:id])
-        if isCreator(current_user.id) == 'true'
-            @group.destroy
-            #flash[:notice] = "group successfully deleted!"
-            #redirect_to(:action => 'form')
-        else
-      	    #flash[:error] = "not allowed"
-            #redirect_to(:action => 'form')
-    end
 end
