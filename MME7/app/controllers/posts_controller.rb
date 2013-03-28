bd1efc4b76a910da48611357edb12b4f77fde097
Riham Gamal <riham.gamal93@gmail.com>
2013-03-28 21:52:23 +0200

    Edit Post

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 class PostsController < ApplicationController
+	
+	# fimd the post you want to edit
 	def editPost
 		@post = Post.find(params[:id])
 	end
 
+	# update the post by finding its id and chnaging the fields
 	def updatePost
 		@post = Post.find(params[:id])
+		rescue_from ActiveRecord::RecordNotFound do
+  		flash[:notice] = 'The object you tried to access does not exist'
+  		render :not_found   # or e.g. redirect_to :action => :index
+		end
+
 		if @post.update_attributes(param[:post])
 			flash[:notice] = "Post successfully updated"
 			render("updatePost")
