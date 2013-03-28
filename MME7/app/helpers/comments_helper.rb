aee4b05ff373ae60f748fed7804f8cfced92712d
nayera-muhammad <nayera.muhammad@gmail.com>
2013-03-28 23:15:01 +0200

    doc is changed

--- MME7/app/helpers/comments_helper.rb
+++ MME7/app/helpers/comments_helper.rb
@@ -1,6 +1,5 @@
 module CommentsHelper
-
-    #Author: Nayera Mohamed 22-3789 , this method sees whether if the this is the comment creator or not
+    #Author: Nayera Mohamed 22-3789, this method sees whether if the this is the comment creator or not
 	
 	def isCommentCreator(current_user, comment_id)
         Comment.where(:user_id => current_user, :comment_id => comment_id, :is_creator => true).exists?  
