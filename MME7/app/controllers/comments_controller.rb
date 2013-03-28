81f8649ff321624c4df49fb74948fd5ba111218f
salmaelruby <salmaelruby@gmail.com>
2013-03-28 02:08:17 +0200

    added method delete comments

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
-end
+# As a system I can delete a comment
++    def destroy
++
++        @c = Comment.find(params[:id])
++        if isCreator(current_user.id) == 'true'
++            @c.destroy
++            flash[:notice] = "Comment successfully deleted!"
++       
++        else
++            flash[:error] = "not allowed"
++          
++    end
+ end
+
