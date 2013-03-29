1c125a9f759c7ca71d7bf5d43013269f60f6b57c
May Atef Badr <may.badr1991@gmail.com>
2013-03-29 21:58:45 +0200

    Delete community updated

--- MME7/app/controllers/communities_controller.rb
+++ MME7/app/controllers/communities_controller.rb
@@ -1,17 +1,19 @@
 class CommunitiesController < ApplicationController
-
+	
 	def list
 
 		@communities = Community.order("communities.title ASC")
 
 	end
 
-
+	#Author: May Badr 22-0579
+	#find record to be deleted
 	def delete 
 		@community = Community.find(params[:id])
 		rescue ActiveRecord::RecordNotFound
 	end
-
+	#Author:May Badr 22-0579
+	#deletes record chosen
 	def destroy 
 		Community.find(params[:id]).destroy
 		rescue ActiveRecord::RecordNotFound
