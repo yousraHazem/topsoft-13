2615197fcf7b769c8a1c65bf4382300f01823b22
salmaelruby <salmaelruby@gmail.com>
2013-03-29 18:12:23 +0200

    added missing files

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+
+  def delete
+    @comment = Comment.find(params[:id])
+  end
+  
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to(:action => 'list')
+  end
+
+    def list 
+  @comment = Comment.all
+    end
+
+
+
+
 end
