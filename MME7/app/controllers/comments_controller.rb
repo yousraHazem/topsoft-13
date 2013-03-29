e3dff07e751d9043420c29646e41f117f54eb63b
salmaelruby <salmaelruby@gmail.com>
2013-03-29 12:37:56 +0200

    add my posts_controller

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+
+
+  def delete
+    @comment = Comment.find(params[:id])
+  end
+  
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to(:action => 'list')
+  end
+
+    def list 
+  @comment = Comment.all
+    end
+
 end
