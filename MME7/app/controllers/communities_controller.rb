7137884bae353ad1b7fae7e8fcabfdc452d47728
May Atef Badr <may.badr1991@gmail.com>
2013-03-29 18:25:56 +0200

    Added delete community method

--- MME7/app/controllers/communities_controller.rb
+++ MME7/app/controllers/communities_controller.rb
@@ -1,12 +1,21 @@
 class CommunitiesController < ApplicationController
 
+	def list
+
+		@communities = Community.order("communities.title ASC")
+
+	end
+
+
 	def delete 
 		@community = Community.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
 	end
 
 	def destroy 
-		Community.find(params[:id].destroy
-		redirect_to (:action =>'application')
+		Community.find(params[:id]).destroy
+		rescue ActiveRecord::RecordNotFound
+		redirect_to(:action => 'list')
+
 	end
-	
 end
