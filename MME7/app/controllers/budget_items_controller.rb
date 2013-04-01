3b5d5d4f12e408db2d214fefeed885aa82480969
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-01 15:57:02 +0200

    new view

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -1,30 +1,16 @@
 class BudgetItemsController < ApplicationController
 
-	#def index
- #     list
- #      render('list')
-	# end 
+	def index
+      list
+       render('list')
+	 end 
 
 
-	# def list	
-	# 	@items = BudgetItem.all
-	# end
+	def list
+        @total_budget = BudgetItem.sum(:total)
+        @total_spent = BudgetItem.sum(:spent)
+		#@items = BudgetItem.where(:project_id=>params[:id])
+	 end
 
-	# def edit
-	# 	@total = BudgetComponent.sum(:total, :conditions => {:budget_item_id =>params[:id]})
-	# 	@item = BudgetItem.find(params[:id])
- #    end
-
- #    def update
-	# 	@item = params[:item_id]
-	# 	@item = Budget.find(params[:id])
-	# 	if @component.update_attributes(params[:budget_component])
-	# 		flash[:notice] = "Component successfully updated"
-	# 		redirect_to(:action=>'list', :id=> @item)
-	# 	else
-	
- #           render('edit')
-	# 	end
-	# end
 end
 
