83909303d361b6bdc3b92ef2b99258a98dc2f71d
mariam <mariamismael@live.com>
2013-04-04 22:27:29 +0200

    save

--- MME7/app/controllers/group_users_controller.rb
+++ MME7/app/controllers/group_users_controller.rb
@@ -1,14 +1,16 @@
   class GroupUsersController < ApplicationController
 
 
-def show
-	
-   @groupuser_id = params[:id]
-	@groupusers = GroupUser.find(params[:id])
-
-
- end
-	
+#Author Mariam Ismail, 22-3456
+#This method adds the user to the group and checks if he's an already exisiting member or not
+  def join
+        @group_id = params[:id]
+       @user_id = params[:user_id]
+       @newuser= GroupUser.new(:group_id => @group_id, :user_id => @user_id)
+       @newuser.save
+     redirect_to(:controller => 'groups', :action => 'show', :id => params[:id])
+    end
+   
    
   end
 
