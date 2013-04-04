944c5819bd0d6e01db0d525482fd788eabb5a31f
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-04 15:30:03 +0200

    new changes to the branch

--- MME7/app/controllers/project_users_controller.rb
+++ MME7/app/controllers/project_users_controller.rb
@@ -1,3 +1,11 @@
 class ProjectUsersController < ApplicationController
-	
+include ProjectUsersHelper
+  #Authored by Toka Omar  id:22-1925
+  #this method takes input :user_id and outputs: list of projects that have the same user_id 
+  #for a user to be able to see their projects 
+  def get_myprojects(userid)
+   @projectusers = ProjectUser.find(:all, 
+   	:conditions => {:user_id => userid}, 
+   	:order => "project_id ASC",)
+    end 
 end
