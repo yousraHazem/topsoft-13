54f36f01848c9aa1ffc82b4cc66c8bfc6948ce2d
May Atef Badr <may.badr1991@gmail.com>
2013-04-06 11:34:40 +0200

    bha

--- MME7/app/controllers/group_users_controller.rb
+++ MME7/app/controllers/group_users_controller.rb
@@ -1,6 +1,18 @@
 class GroupUsersController < ApplicationController
-	def addMember
+	def listMembers
 		@group_id = params[:id]
 		@users= User.find(:all)
+	end
+
+	def addMembers
+		@group_id = params[:group_id]
+		@user_id= params[:user_id]
+		@new_user= GroupUser.new(:group_id => @group_id, :user_id => @user_id)
+
+		 if @new_user.save
+           redirect_to(:action => "listMembers", :id => @group_id)
+        else
+            render ('addMembers')
+        end
 	end
 end 
