0a992472fe944c49d5b50d83483aead0d5da47a9
salmaelruby <salmaelruby@gmail.com>
2013-04-08 00:07:20 +0200

    added uml of isCreator and post helper of post creator

--- MME7/app/helpers/posts_helper.rb
+++ MME7/app/helpers/posts_helper.rb
@@ -1,5 +1,6 @@
 module PostsHelper
-	#method to check if the user is the owner of this post
+	#Author :Salma El Ruby 22-4649 
+	#method to check if the user is the owner of this post, takes as parameters the user id and the post id and returns boolean 
 	def isCreator (current_user, post_id)
 		Post.where(:user_id => current_user, :post_id => post_id ).exists?
 end
