2a2be1cc8d5aef489d5cefef517efbc7d7d21387
May Atef Badr <may.badr1991@gmail.com>
2013-04-07 20:53:08 +0200

    modified method#162

--- MME7/app/models/community.rb
+++ MME7/app/models/community.rb
@@ -4,13 +4,19 @@ class Community < ActiveRecord::Base
    has_many :groups
    validates_presence_of :title
 
-   def self.search(search)
-	if search
-		find(:all, :conditions => ['title LIKE ?',"%#{search}%"])
-	else 
-		find(:all)
+   # May Atef Badr 22-0579
+   # This method takes the input the user types in the search field
+   # and compares it with the titles of all communities
+   # if the title matches the input it will list the results of the filtering
+   # else it won't list anything
+   
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['title LIKE ?',"%#{search}%"])
+		else 
+			find(:all)
+		end
 	end
-end
 
 
 end
