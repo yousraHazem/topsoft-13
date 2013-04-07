e45a9a679761720bc7fa6ff2f2a1d5057dcfd6a4
mariam <mariamismael@live.com>
2013-04-07 19:50:37 +0200

    added the post controller changes

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -1,33 +1,34 @@
 class PostsController < ApplicationController
- #  def delete
- #    @post = Post.find(params[:id])
- #  end
-  
- #  def destroy
- #    Post.find(params[:id]).destroy
- #    redirect_to(:action => 'list')
- #  end
+include PostsHelper
+ 
+ 	def index
+        @posts = Post.order("posts.content ASC")
+    end
 
- #    def list 
-	# @post = Post.all
- #    end
 
- 	# Riham Gamal 22-3871
+   # Riham Gamal 22-3871
  	# add a new post 
+
 	def newPost
+		@group_id = params[:id]
 		@post = Post.new
 	end
 
-	#Author Riham Gamal 22-3871
+	#Author Riham Gamal 22-3871, Mariam Ismail 22-3456
 	# create a new post
+	# if the post is saved, added to this method the group-id 
 	def createPost
+
 		@post = Post.new(params[:post])
 
-		#Author Riham Gamal 22-3871
-		# if the post is saved, 
+
 		if @post.save
 			flash[:notice] = "Post successfully created"
-			redirect_to(:action => 'newPost')
+			#redirect_to(:controller => 'groups',:action => 'show', :id =>params[:id])
+			respond_to do |format|
+				format.html{redirect_to(:controller => 'groups',:action => 'show', :id =>params[:id])}
+				format.js
+			end
 		#Author Riham Gamal 22-3871
 		# if the post is not saved, 
 		else
@@ -36,27 +37,38 @@ class PostsController < ApplicationController
 		end
 
 	end
-
-
 	
 	# Riham Gamal 22-3871
 	# find the post you want to edit
-	def editPost
+
+	def editPost	
+	@group_id = params[:id]
 		@post = Post.find(params[:id])
-	end
+	#else
+		#render('problempost')
+	#end
+end
 
 	# Riham Gamal 22-3871
 	# update the post by finding its id and changing the fields
+
+
 	def updatePost
-		@post = Post.find(params[:id])
-		
+		 @post = Post.find(params[:id])
+        
 		if @post.update_attributes(params[:post])
+
 			flash[:notice] = "Post successfully updated"
-			render("editPost")
+			redirect_to(:controller => 'groups',:action => 'show', :group_id => @group_id)
 		else
+
 			flash[:notice] = "Post could not be updated"
-			# render("editPost")
+			 render("editPost")
 		end
+
+
+
+
 	end
 
 	# # add a new post 
@@ -72,15 +84,23 @@ class PostsController < ApplicationController
 	  
 	  def destroy
 	    Post.find(params[:id]).destroy
+	    flash[:notice] = "Post successfully deleted"
 	    redirect_to(:action => 'list')
 	  end
+
+
+# Salma El Ruby 22-4649
+# displays all available posts 
+    def list 
+	@post = Post.all
+	# Post.getposts (32)
+       
+    end
 
 		# Salma El Ruby 22-4649
 		# displays all available posts 
 	    def list 
 		@post = Post.all
 	    end
-
-		
 
 end
