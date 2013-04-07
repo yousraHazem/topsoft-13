e6013d26e664868ab22da8e472ce4b3901a144dc
salmaelruby <salmaelruby@gmail.com>
2013-04-08 00:34:03 +0200

    added tasks and users controllers

--- MME7/app/controllers/users_controller.rb
+++ MME7/app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+ def assign
+  @task_id = params[:task_id]
+  @projectid = params[:id]
+@userid = params[:user_id]
+
+   @newUser=TaskUser.create(:user_id => @userid , :task_id => @task_id)
+
+
+   redirect_to(:controller => 'tasks' ,:action => 'getProjectMembers' , :id => @task_id , :project_id => @projectid)
+
+   
+
+end 
+def index
+	@user = User.where("name like ?", "%#{params[:q]}%")
+	
+ # @user = User.where("name like ?", "%#{params[:q]}%")
+  respond_to do |format|
+    format.html
+    format.json { render :json => @user.map(&:attributes) }
+  end
+end
 end 
