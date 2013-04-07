4aa043083ba62ee54a4c188135611c0e184e7b82
salmaelruby <salmaelruby@gmail.com>
2013-04-08 01:11:13 +0200

    added the auto complete search with assign members to task in a specific project

--- MME7/app/helpers/tasks_helper.rb
+++ MME7/app/helpers/tasks_helper.rb
@@ -4,4 +4,17 @@ module TasksHelper
 	def get_tasks (p_id)
 	return Task.find(:all, :conditions => {:project_id =>p_id})
     end
+    #Author by Salma El Ruby 22-4649
+  	#this method takes input :project id and outputs: ids of members in this project 
+
+	def get_projectmembers(project_id)
+ 	     @projectmembersid = ProjectUser.find(:all, :conditions => {:project_id => project_id })
+    end
+    
+    def isCreator (current_user, project_id)
+		ProjectUser.where(:user_id => current_user, :project_id => project_id ).exists?
+  end
+
+
+
 end
