c9e6e256ba106907337dc52844d280a3668b9ef0
salmaelruby <salmaelruby@gmail.com>
2013-04-08 00:51:48 +0200

    added my edit html page with its controller

--- MME7/app/models/task.rb
+++ MME7/app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ActiveRecord::Base
-  attr_accessible :description , :project_id
+  attr_accessible :description , :project_id ,:user_tokens
 has_one :budget_item 
-  has_many :users 
+  has_many :task_users 
   belongs_to :project 
+  has_many :users , :through => :task_users
+  attr_reader :user_tokens
+  
+  def user_tokens=(ids)
+    self.user_ids = ids.split(",")
+  end
+
 end
