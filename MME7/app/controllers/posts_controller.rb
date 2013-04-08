4ecda346486f01f5959521a86b91574f985fcdea
mariam <mariamismael@live.com>
2013-04-08 11:45:01 +0200

    update post action

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -50,20 +50,30 @@ class PostsController < ApplicationController
 		
 end
 
-	# Riham Gamal 22-3871
+	
+	# Riham Gamal 22-3871, Mariam Ismail 22-3456
 	# update the post by finding its id and changing the fields
-	def updatePost
-		@post = Post.find(params[:id])
-		
-		if @post.update_attributes(params[:post])
-			flash[:notice] = "Post successfully updated"
-			render("editPost")
-		else
-			flash[:notice] = "Post could not be updated"
-			# render("editPost")
-		end
-	end
+	# takes the group-id and redirects back to show group
 
+
+	def updatePost
+		 @post = Post.find(params[:post_id])
+		 @group_id = params[:group_id]
+	    @post_id=params[:post_id]
+        
+		if @post.update_attributes(params[:post])
+
+			flash[:notice] = "Post successfully updated"
+			redirect_to(:controller => 'groups',:action => 'show', :id => params[:group_id])
+		else
+
+			flash[:notice] = "Post could not be updated"
+			 
+			redirect_to(:action => 'editPost',:id =>@post_id, :group_id =>@group_id)
+		end
+
+
+	end
 	# # add a new post 
 	# def newPost
 	# 	@post = Post.new
