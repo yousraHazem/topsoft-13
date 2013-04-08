b28155ec15ee1e1d677d657210bc47c3c4dde540
mariam <mariamismael@live.com>
2013-04-08 22:12:50 +0200

    save

--- MME7/spec/controllers/communities_controllers_spec.rb
+++ MME7/spec/controllers/communities_controllers_spec.rb
@@ -17,48 +17,6 @@ describe CommunitiesController do
     end
   end
 
- describe "GET new" do
-    it "assigns a new community as @community" do
-      get :new
-      assigns(:community).should be_a_new(Community)
-    end
-  end
-describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Commuity" do
-        expect {
-          post :create, {:community => valid_attributes}
-        }.to change(Community, :count).by(1)
-      end
-
-      it "assigns a newly created community as @community" do
-        post :create, {:community => valid_attributes}
-        assigns(:community).should be_a(Community)
-        assigns(:community).should be_persisted
-      end
-
-      it "redirects to the created community" do
-        post :create, {:community => valid_attributes}
-        response.should redirect_to("http://test.host/communities/show/1")
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved community as @community" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Community.any_instance.stub(:save).and_return(false)
-        post :create, {:community => valid_attributes}
-        assigns(:community).should be_a_new(Community)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Community.any_instance.stub(:save).and_return(false)
-        post :create, {:community => valid_attributes}
-        response.should render_template("new")
-      end
-    end
-  end
 
   describe "PUT update" do
     describe "with valid params" do
@@ -80,7 +38,7 @@ describe "POST create" do
       it "redirects to the community" do
         community = Community.create! valid_attributes
         put :update, {:id => community.to_param, :community => valid_attributes}
-        response.should redirect_to("http://test.host/communities/show/1")
+        response.should redirect_to("http://test.host/communities/list")
       end
     end
 
