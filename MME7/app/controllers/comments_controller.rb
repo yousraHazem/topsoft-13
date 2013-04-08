69f4ff889584698b86d28445eebde64f002d7666
sama akram abdelrahman <samaakram92@gmail.com>
2013-04-08 15:43:14 +0200

    h

--- MME7/app/controllers/comments_controller.rb
+++ MME7/app/controllers/comments_controller.rb
@@ -3,16 +3,15 @@ class CommentsController < ApplicationController
   #Author Riham Gamal 22-3871
   # create a new comment
   # sama updated
-  #Author: Sama Akram 22-555 made the dependency with post so that i can only comment on a specific post
+  #Author: Sama Akram 22-555 
+  #redirection changed to
   def createComment
     @comment = Comment.new(params[:comment])
     #Author Riham Gamal 22-3871
     # if the comment is saved, 
     if @comment.save
       flash[:notice] = "Comment successfully created"
-      respond_to do |format|
-        format.html{redirect_to(:controller => 'groups',:action => 'show', :id =>params[:id])}
-      end
+      redirect_to(:controller => 'groups',:action => 'show', :id => params[:id])
       #redirect_to(:controller => 'groups',:action => 'show', :id => @group_id)
       #Author Riham Gamal 22-3871
       # if the comment is not saved, 
@@ -24,20 +23,19 @@ class CommentsController < ApplicationController
 
   # Riham Gamal 22-3871
   def editComment
-    @comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:comment_id])
   end
 
   # Riham Gamal 22-33871
   # update the comment by finding its id and chnaging the fields
   # sama updates
-  # Author: Sama Akram 22-555 same concept as new we find the post id first that has the comment
-  # the user wants to edit
+  # Author: Sama Akram 22-555 
+  # redirection updates
   def updateComment
-    @comment = Comment.find(params[:id])
-    
+    @comment = Comment.find(params[:comment_id])
     if @comment.update_attributes(params[:comment])
       flash[:notice] = "Comment successfully updated"
-      redirect_to :action => 'list'
+      redirect_to(:controller => 'groups',:action => 'show', :id => params[:id])
     else
       flash[:notice] = "Comment could not be updated"
       render("editComment")
@@ -50,16 +48,16 @@ class CommentsController < ApplicationController
 	
   # Salma El -Ruby 22-4649
   # deletes comments
-  def delete
-    @comment = Comment.find(params[:id])
-  end
+  #def delete
+   # @comment = Comment.find(params[:id])
+  #end
   
-  # Author: Sama Akram 22-555 we find the post id first that has the comment
-  # the user wants to edit
+  # Author: Sama Akram 22-555 
+  # updated the redirection
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to(:action => 'list')
+    redirect_to(:controller => 'groups', :action => 'show', :id => @group.id)
   end
 
   # Salma El -Ruby 22-4649
