2e3cfdfc682f4c74eee5f4ab7076e4f50f9eff67
mariam <mariamismael@live.com>
2013-04-08 11:04:02 +0200

    save

--- MME7/app/models/group.rb
+++ MME7/app/models/group.rb
@@ -9,7 +9,7 @@ class Group < ActiveRecord::Base
 
 	#Authored by Toka Omar  id:22-1925
   	#this method takes input :group id and outputs: posts that have the same group id entered 
-	def getposts (g_id)
+	def self.getposts (g_id)
 	return Post.find(:all, :conditions => {:group_id =>g_id})
     end
 
