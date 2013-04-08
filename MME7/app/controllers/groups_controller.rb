c39252ae09c3c430653b6e820daf682c50154248
mariam <mariamismael@live.com>
2013-04-08 09:15:12 +0200

    getposts of that group

--- MME7/app/controllers/groups_controller.rb
+++ MME7/app/controllers/groups_controller.rb
@@ -19,8 +19,9 @@ def index
     #end
 
     def show
+         @group_id = params[:id]
         @group = Group.find(params[:id])
-        rescue ActiveRecord::RecordNotFound
+        @postlist=Group.getposts (params[:id])
     end
 
     # Author: Sama Akram 22-555 As a system I can create a group, calls when clicked on create new group
