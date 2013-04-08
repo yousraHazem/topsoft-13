27264e5c9d92d12b166ebc9f56d61cd34ed97f9b
mariam <mariamismael@live.com>
2013-04-08 09:17:42 +0200

    added getposts method

--- MME7/app/models/group.rb
+++ MME7/app/models/group.rb
@@ -6,6 +6,11 @@ class Group < ActiveRecord::Base
   has_many :groups_users 
   has_many :users , :through => :group_users
 
+	#Authored by Toka Omar  id:22-1925
+  	#this method takes input :group id and outputs: posts that have the same group id entered 
+	def getposts (g_id)
+	return Post.find(:all, :conditions => {:group_id =>g_id})
+    end
 
 
 end 
