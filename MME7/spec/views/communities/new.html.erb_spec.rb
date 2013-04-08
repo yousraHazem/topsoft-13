ccf82b59aa916a5f2d2a938e717d57f26e03ad48
mariam <mariamismael@live.com>
2013-04-08 20:18:42 +0200

    added test for new template

--- MME7/spec/views/communities/new.html.erb_spec.rb
+++ MME7/spec/views/communities/new.html.erb_spec.rb
@@ -1,17 +1,15 @@
+
+
 require 'spec_helper'
- 
+
 describe "communities/new.html.erb" do
-  before(:each) do
-    @post = assign(:community, stub_model(Community)).as_new_record.as_null_object
- end
-  it "renders the new community" do
-    rendered.should have_selector('form',:method => "post",:action => 'new') do |form|
-      form.should have_selector('label',:for=>'community_title',:content=>'Title')
-      form.should have_selector('input',:type => "string",:name=>'community[title]',:id=>'community_title')
-      form.should have_selector('label',:for=>'community_social_profile_info',:content=>'Social_profile_info')
-      form.should have_selector('labe',:type=>'text',:name=>'community[social_profile_info]',:id=>'community_social_profile_info')
-      form.should have_selector('input',:type=>'submit',:value=>'Publish')
+    before(:each) do
+      view.stub(:community).and_return(mock_model(Community))
     end
-  end
- 
+
+    it 'renders a form' do
+     render
+     expect(view).to render_template(:form => "show")
+    end
+    
 end
