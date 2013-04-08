9432680113c2339949f102e34c43d32a0b41b7ba
mariam <mariamismael@live.com>
2013-04-08 20:54:08 +0200

    test for controller

--- MME7/spec/controllers/communities_controllers_spec.rb
+++ MME7/spec/controllers/communities_controllers_spec.rb
@@ -7,15 +7,15 @@ describe CommunitiesController do
   end
 
   def invalid_attributes
-   {:title => "", :social_profile_info => "knnn", :contact_info => "jjjj"}
+   {:title => "", :social_profile_info => "", :contact_info => "jjjj"}
   end
- describe "GET edit" do
-    it "assigns the requested community as @community" do
-      community = Community.create! valid_attributes
-      get :edit, {:id => community.to_param}
-      assigns(:community).should eq(community)
-    end
-  end
+ #describe "GET edit" do
+ #   it "assigns the requested community as @community" do
+  #    community = Community.create! valid_attributes
+   #   get :edit, {:id => community.to_param}
+    #  assigns(:community).should eq(community)
+    #end
+  #end
 
  describe "GET new" do
     it "assigns a new community as @community" do
@@ -27,19 +27,19 @@ describe "POST create" do
     describe "with valid params" do
       it "creates a new Commuity" do
         expect {
-          post :create, {:community => valid_attributes}
+          post :createCommunity, {:community => valid_attributes}
         }.to change(Community, :count).by(1)
       end
 
       it "assigns a newly created community as @community" do
-        post :create, {:community => valid_attributes}
+        post :createCommunity, {:community => valid_attributes}
         assigns(:community).should be_a(Community)
         assigns(:community).should be_persisted
       end
 
-      it "redirects to the created community" do
-        post :create, {:community => valid_attributes}
-        response.should redirect_to("http://test.host/communities/show/1")
+      it "redirects to the community list" do
+        post :createCommunity, {:community => valid_attributes}
+        response.should redirect_to("http://test.host/communities/list")
       end
     end
 
@@ -47,61 +47,19 @@ describe "POST create" do
       it "assigns a newly created but unsaved community as @community" do
         # Trigger the behavior that occurs when invalid params are submitted
         Community.any_instance.stub(:save).and_return(false)
-        post :create, {:community => valid_attributes}
+        post :createCommunity, {:community => valid_attributes}
         assigns(:community).should be_a_new(Community)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Community.any_instance.stub(:save).and_return(false)
-        post :create, {:community => valid_attributes}
+        post :createCommunity, {:community => valid_attributes}
         response.should render_template("new")
       end
     end
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested community" do
-        community = Community.create! valid_attributes
-        # Assuming there are no other <%= table_name %> in the database, this
-        # specifies that the <%= class_name %> created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        put :update, {:id => community.to_param, :community => valid_attributes}
-      end
-
-      it "assigns the requested community as @community" do
-        community = Community.create! valid_attributes
-        put :update, {:id => community.to_param, :community => valid_attributes}
-        assigns(:community).should eq(community)
-      end
-
-      it "redirects to the community" do
-        community = Community.create! valid_attributes
-        put :update, {:id => community.to_param, :community => valid_attributes}
-        response.should redirect_to("http://test.host/communities/show/1")
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the community as @community" do
-        community = Community.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Community.any_instance.stub(:save).and_return(false)
-        put :update, {:id => community.to_param, :community => valid_attributes}
-        assigns(:community).should eq(community)
-      end
-
-      it "re-renders the 'edit' template" do
-        community = Community.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Community.any_instance.stub(:save).and_return(false)
-        put :update, {:id =>community.to_param, :community => invalid_attributes}
-        response.should render_template("edit")
-      end
-    end
-  end
 end
 
 
