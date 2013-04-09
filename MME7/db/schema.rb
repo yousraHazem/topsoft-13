10987873a573b38238c573ab986f1c05f61bea42
mariam <mariamismael@live.com>
2013-04-09 23:16:07 +0200

    db

--- MME7/db/schema.rb
+++ MME7/db/schema.rb
@@ -59,6 +59,22 @@ ActiveRecord::Schema.define(:version => 20130408104319) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "budget_sources_projects", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "budget_source_id"
+  end
+
+  create_table "budgets", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "amount"
+    t.integer  "raised"
+    t.integer  "noot_raised"
+    t.integer  "spent"
+    t.integer  "not_spent"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
     t.integer  "user_id"
@@ -97,6 +113,13 @@ ActiveRecord::Schema.define(:version => 20130408104319) do
     t.integer  "community_id"
   end
 
+  create_table "groups_users", :id => false, :force => true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
+
+  add_index "groups_users", ["group_id", "user_id"], :name => "index_groups_users_on_group_id_and_user_id"
+
   create_table "posts", :force => true do |t|
     t.text     "content"
     t.integer  "group_id"
@@ -124,6 +147,13 @@ ActiveRecord::Schema.define(:version => 20130408104319) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
+
+  create_table "projects_users", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
+
+  add_index "projects_users", ["project_id", "user_id"], :name => "index_projects_users_on_project_id_and_user_id"
 
   create_table "receipts", :force => true do |t|
     t.string   "name"
