6a0ff4ed3511a8ccad74d1a0411cefdfcfbb613e
nayera-muhammad <nayera.muhammad@gmail.com>
2013-04-09 21:10:25 +0200

    adjusted comment model and the tests#189

--- MME7/app/models/comment.rb
+++ MME7/app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ActiveRecord::Base
 
     attr_accessible :post_id , :user_id , :comment 
-    # Author : Nayera Mohamed 22-3789 , this method takes as an input a user id and sees whether this user has made this comment or not (returns boolean)
-    def isCommentCreator(current_user)
-        Comment.where(:user_id => current_user).exists?  
+    #Author : Nayera Mohamed 22-3789, this method takes the user id and the comment id and reutrns a boolean stating whether this is the comment creator or not
+    def isCommentCreator(current_user, comment_id)
+        Comment.where(:user_id => current_user, :id => comment_id).exists?  
     end
 
 	belongs_to :user #comments creator
