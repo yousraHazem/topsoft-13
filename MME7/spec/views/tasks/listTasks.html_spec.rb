b16bfbcf6008467aca6c2a66b4258cc2b29fcc8f
nayera-muhammad <nayera.muhammad@gmail.com>
2013-04-09 13:34:15 +0200

    added and adjusted tasks#178

---
+++ MME7/spec/views/tasks/listTasks.html_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "tasks/listTasks.html" do
+  it "renders the task partial for each project" do
+    assign(:tasks,[
+      mock_model(Task, :description => 'pr' , :project_id => 1),
+      mock_model(Task, :description => 'hr' , :project_id => 2)
+    ])
+    render
+    rendered.should =~ /pr/
+    rendered.should =~ /hr/
+
+  end
+end
