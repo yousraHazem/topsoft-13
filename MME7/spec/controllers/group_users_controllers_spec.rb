05892be7db0e4121e1d17c0e890d52d89b9a223d
mariam <mariamismael@live.com>
2013-04-09 15:36:39 +0200

    save

--- MME7/spec/controllers/group_users_controllers_spec.rb
+++ MME7/spec/controllers/group_users_controllers_spec.rb
@@ -1,63 +1,36 @@
-require 'spec_helper'
-
-describe GroupUsersController do
-
-  def valid_attributes
-   {:content=> "put valid attributes"}
-  end
-
-  def invalid_attributes
-   {:content => "", :social_profile_info => "", :contact_info => "jjjj"}
-  end
- #describe "GET edit" do
- #   it "assigns the requested community as @community" do
-  #    community = Community.create! valid_attributes
-   #   get :edit, {:id => community.to_param}
-    #  assigns(:community).should eq(community)
-    #end
-  #end
-
- describe "GET new" do
-    it "assigns a new groupuser as @groupuser" do
-      get :join
-      assigns(:community).should be_a_new(Community)
-    end
-  end
 describe "join" do
     describe "with valid params" do
-      it "creates a new Commuity" do
+      it "creates a new groupuser" do
         expect {
           post :join, {:groupuser => valid_attributes}
         }.to change(GroupUser, :count).by(1)
       end
 
-      it "assigns a newly created community as @community" do
-        post :join, {:groupuser => valid_attributes}
-        assigns(:groupuser).should be_a(Community)
+      it "assigns a newly created groupuser as @groupuser" do
+        groupuser :groupuser, {:groupuser => valid_attributes}
+        assigns(:groupuser).should be_a(GroupUser)
         assigns(:groupuser).should be_persisted
       end
 
-      it "redirects to the group show" do
+      it "redirects to the group show " do
         post :join, {:groupuser => valid_attributes}
-        response.should redirect_to(:controller => "groups", :action => 'show')
+        response.should redirect_to(:controller => 'groups',:action => "show")
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved community as @community" do
+      it "assigns a newly created but unsaved post as @groupuser" do
         # Trigger the behavior that occurs when invalid params are submitted
-        Community.any_instance.stub(:save).and_return(false)
-        post :createCommunity, {:community => valid_attributes}
-        assigns(:community).should be_a_new(Community)
+        GroupUser.any_instance.stub(:save).and_return(false)
+        post :join, {:groupuser => valid_attributes}
+        assigns(:groupuser).should be_a_new(GroupUser)
       end
 
-      it "re-renders the 'new' template" do
+      it "re-renders the show group template" do
         # Trigger the behavior that occurs when invalid params are submitted
-        Community.any_instance.stub(:save).and_return(false)
-        post :createCommunity, {:community => valid_attributes}
-        response.should render_template("new")
+        GroupUser.any_instance.stub(:save).and_return(false)
+        post :join, {:GroupUser => valid_attributes}
+    response.should redirect_to(:controller => 'groups',:action => "show")
       end
     end
   end
-
-end
