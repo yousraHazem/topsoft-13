c4f06e5d03790c6adfa961889fc86e63ad5fa136
mariam <mariamismael@live.com>
2013-04-10 01:11:41 +0200

    modified

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -38,11 +38,6 @@ class PostsController < ApplicationController
 
 
 	
-	# Riham Gamal 22-3871
-	# find the post you want to edit
-	def editPost
-		@post = Post.find(params[:id])
-	end
 
 	# Riham Gamal 22-3871
 	# update the post by finding its id and changing the fields
@@ -63,12 +58,7 @@ class PostsController < ApplicationController
 	# 	@post = Post.new
 	# end
 
-	# Salma El Ruby 22-4649
-	# deletes posts
-	  def delete
-	    @post = Post.find(params[:id])
-	  end
-	  
+
 	  def destroy
 	    Post.find(params[:id]).destroy
 	    redirect_to(:action => 'list')
