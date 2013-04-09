d81c3122d4dac99a277b29e3cab72dcd3e7c5abd
May Atef Badr <may.badr1991@gmail.com>
2013-04-09 16:47:46 +0200

    view Groups testControl#158

--- MME7/spec/controllers/groups_controller_spec.rb
+++ MME7/spec/controllers/groups_controller_spec.rb
@@ -5,11 +5,13 @@ describe GroupsController do
   def valid_attributes
       {:group_name => 'groupOne', :description => 'descTwo', :levels => 'parent'}
   end
+
   describe "GET index" do
     it "assigns all groups as @groups" do
       group = Group.create! valid_attributes
-      #puts community
-      get : index, {}
+     
+      get :index, {}
       #assigns(Community).should eq([community])
     end
   end
+end
