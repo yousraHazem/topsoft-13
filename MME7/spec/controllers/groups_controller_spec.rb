50b60627c95131e10cd254695fb256b3b4d98960
May Atef Badr <may.badr1991@gmail.com>
2013-04-09 15:57:07 +0200

    view groupsf

---
+++ MME7/spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe GroupsController do
+
+  def valid_attributes
+      {:group_name => 'groupOne', :description => 'descTwo', :levels => 'parent'}
+  end
+  describe "GET index" do
+    it "assigns all groups as @groups" do
+      group = Group.create! valid_attributes
+      #puts community
+      get : index, {}
+      #assigns(Community).should eq([community])
+    end
+  end
