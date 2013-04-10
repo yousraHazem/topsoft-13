2c95492d6644d25837de56efcb7921d9075c0965
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-10 16:53:23 +0200

    JFJF

--- MME7/app/controllers/projects_controller.rb
+++ MME7/app/controllers/projects_controller.rb
@@ -57,6 +57,8 @@ end
   :project_id => @id})   #projectcheck
   @project_name = Project.find(@id)
     end
+
+    
   #Authored by Toka Omar  id:22-1925
   #this method has no inputs and outputs: the all  budgetitems 
   #the method returs a list of operational budgetitems the admin or user can view 
