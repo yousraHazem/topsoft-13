efbb81fac24af1d9c9e9ab7bcfef0c1633871cad
May Atef Badr <may.badr1991@gmail.com>
2013-04-10 12:02:20 +0200

    edit group#165

--- MME7/spec/controllers/groups_controller_spec.rb
+++ MME7/spec/controllers/groups_controller_spec.rb
@@ -7,7 +7,7 @@ describe GroupsController  do
  	end
 
  	def invalid_attributes
- 		{:group_name => "groupOne",:description => "", :levels => ""}
+ 		{:group_name => "",:description => "", :levels => ""}
  	end
 
   def update_attributes
@@ -17,53 +17,51 @@ describe GroupsController  do
 
 	describe "GET edit" do
    		 it "assigns the requested group as @group" do
-			 group  = Group.create! valid_attributes
-     		 get :edit, {:id => group.to_param}
-     		 assigns(:group).should eq(group)
+  			 group  = Group.create! valid_attributes
+       		 get :edit, {:id => group.to_param}
+       		 assigns(:group).should eq(group)
    		 end
- 	 end
-
- 	 describe "with invalid params" do
-      it "assigns a newly created but unsaved group as @group" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Group.any_instance.stub(:save).and_return(false)
-        post :create, {:group => invalid_attributes}
-        assigns(:group).should be_a_new(Group)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Group.any_instance.stub(:save).and_return(false)
-        post :create, {:group => invalid_attributes}
-        response.should render_template("new")
-      end
-    end
   end
 
- 	 describe "PUT update" do
-   		 describe "with valid params" do
-      it "updates the requested group" do
-        group = Group.create! valid_attributes
-        puts group
-        puts "HAHAHAHHAHA"
-        # Assuming there are no other <%= table_name %> in the database, this
-        # specifies that the <%= class_name %> created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        put :update, {:id => group.to_param, :group => update_attributes}
-      end
+    describe "PUT update" do
+            describe "with valid params" do
+                it "updates the requested group" do
+                    group = Group.create! valid_attributes
+                    put :update, {:id => group.to_param, :group => update_attributes }
+                end
 
-      it "assigns the requested group  as @group" do
-        group = Group.create! valid_attributes
-        put :update, {:id => group.to_param, :group  => valid_attributes}
-        assigns(:group).should eq(group)
-      end
+                it "assigns the requested group as @group" do
+                    group = Group.create! valid_attributes
+                    put :update, {:id => group.to_param, :group => valid_attributes}
+                    assigns(:group).should eq(group)
+                end
 
-      it "redirects to the group" do
-        group = Group.create! valid_attributes
-        put :update, {:id => group.to_param, :@group => valid_attributes}
-        response.should redirect_to(group)
-      end
+                it "redirects to the group" do
+                    group = Group.create! valid_attributes
+                    put :update, {:id => group.to_param, :@group => valid_attributes}
+                    response.should redirect_to( 'http://test.host/groups')
+                end
+
+            end
+
+            describe "with invalid params" do
+                it "assigns the group as @group" do
+                    group = Group.create! valid_attributes
+                    # Trigger the behavior that occurs when invalid params are submitted
+                    Group.any_instance.stub(:save).and_return(false)
+                    put :update, {:id => group.to_param, :group => invalid_attributes}
+                    assigns(:group).should eq(group)
+                end
+                it "re-renders the 'edit' template" do
+                    group = Group.create! valid_attributes
+                    # Trigger the behavior that occurs when invalid params are submitted
+                    Group.any_instance.stub(:save).and_return(false)
+                    put :update, {:id => group.to_param, :group => invalid_attributes}
+                    response.should render_template(:action => "edit")
+                end
+            end
     end
-  
-	end
+
+
+
+end
