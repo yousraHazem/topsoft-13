0f5a5c4db3cc3c0fe2982846e929d00e7609c7c8
mariam <mariamismael@live.com>
2013-04-11 01:17:04 +0200

    sama+mariam merging

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -47,9 +47,8 @@ class PostsController < ApplicationController
 
 
 	  def destroy
-		@group_id = params[:group_id]
-	    @post = Post.find(params[:post_id])
-	    @post.destroy
+		 @group_id = params[:group_id]
+	    Post.find(params[:id]).destroy
 	    redirect_to(:controller => 'groups',:action => 'show', :id => params[:group_id])
 	  end
 		# Salma El Ruby 22-4649
