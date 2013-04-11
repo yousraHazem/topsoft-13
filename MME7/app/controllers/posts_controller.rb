84cdfb6420c39cbbc83fc634e5c2a1c069c343ed
mariam <mariamismael@live.com>
2013-04-11 03:37:26 +0200

    merging done with sama

--- MME7/app/controllers/posts_controller.rb
+++ MME7/app/controllers/posts_controller.rb
@@ -1,18 +1,4 @@
 class PostsController < ApplicationController
- #  def delete
- #    @post = Post.find(params[:id])
- #  end
-  
- #  def destroy
- #    Post.find(params[:id]).destroy
- #    redirect_to(:action => 'list')
- #  end
-
- #    def list 
-	# @post = Post.all
- #    end
-
-
 	#Author Riham Gamal 22-3871
 	# create a new post
 	def createPost
@@ -26,10 +12,7 @@ class PostsController < ApplicationController
 		else
 			redirect_to(:controller => 'groups',:action => 'show', :id =>params[:id])
 		end
-
 	end
-
-
 	# Riham Gamal 22-3871, Mariam Ismail 22-3456
 	# update the post by finding its id and changing the fields
 	# takes the group-id and redirects back to show group
@@ -43,18 +26,17 @@ class PostsController < ApplicationController
 		 redirect_to(:controller => 'groups',:action => 'show', :id => params[:group_id])
 	end
 
-
-
-
-	  def destroy
+	def destroyPost
 		 @group_id = params[:group_id]
-	    Post.find(params[:id]).destroy
+     @post=Post.find(params[:post_id])
+	    @post.destroy
+
 	    redirect_to(:controller => 'groups',:action => 'show', :id => params[:group_id])
-	  end
+	end
 		# Salma El Ruby 22-4649
 		# displays all available posts 
-	    def list 
+	def list 
 		@post = Post.all
-	    end	
+	end	
 
 end
