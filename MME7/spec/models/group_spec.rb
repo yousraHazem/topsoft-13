f2ec960a3146ea9c8d8e947afcb609da3bbe8243
Donia Amer Shaarawy <shaarawy.donia@gmail.com>
2013-04-11 19:06:29 +0200

    signup good#155

--- MME7/spec/models/group_spec.rb
+++ MME7/spec/models/group_spec.rb
@@ -1,17 +1,4 @@
 ﻿require 'spec_helper'
-describe GroupUsersController do
-#Author: Donia Amer Shaarawy 22-0270
-#test for members in a group 
-it"should return list of members in a group" do
-group = Group.new(:group_name => "group",)
-group.save
-groupid = group.id
-groupuser = GroupUser.new(:group_id => 1 , :user_id => 1, :isCreator => false)
-groupuser.save
-groupuser = GroupUser.new(:group_id => 1 , :user_id => 2, :isCreator => false)
-gropuser.save
-groupuser.getGroupMembers(group_id)
-end
-   
-
+ describe Group do
+ 
 end
