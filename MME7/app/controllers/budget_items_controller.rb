0e4b3914549b253f139a6e0f8f0e94cc9f4708bb
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-11 22:04:17 +0200

    changed place of method and added test

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -1,2 +1,12 @@
 class BudgetItemsController < ApplicationController
+ #Authored by Toka Omar  id:22-1925
+  #this method has no inputs and outputs: the all  budgetitems 
+  #the method returs a list of operational budgetitems the admin or user can view 
+  def viewAllBI 
+        #@id = params[:id]
+    	@budget_items = BudgetItem.find(:all, 
+    		:conditions => {:project_id => 1})
+    	#@project_name = Project.find(params[:id])
+    end
+
 end
