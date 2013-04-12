bdcfcfba602a9da3879e19b13dcd44cf821dc49a
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-12 17:52:01 +0200

    fixed code

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -1,12 +1,10 @@
 ﻿class BudgetItemsController < ApplicationController
- #Authored by Toka Omar  id:22-1925
+  #Authored by Toka Omar  id:22-1925
   #this method has no inputs and outputs: the all  budgetitems 
   #the method returs a list of operational budgetitems the admin or user can view 
   def viewAllBI 
-        #@id = params[:id]
-    	@budget_items = BudgetItem.find(:all, 
-    		:conditions => {:project_id => 1})
+    	@budget_items = BudgetItem.find(:all,:conditions => {:project_id => 1})
     	#@project_name = Project.find(params[:id])
-    end
+  end
 
 end
--- MME7/spec/controllers/budget_items_controller_spec.rb
+++ MME7/spec/controllers/budget_items_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe BudgetItemsController do
-
+  #Authored by:Toka Abdelgabar 22-1925
   def valid_attributes
       {:name => 'comOne', :status => 'false', 
        :operational => 'true',:project_id =>'1'}
   end
+  #Authored by:Toka Abdelgabar 22-1925
   describe "GET viewAllBI" do
     it "assigns all budget_items as @budget_items" do
       budget_item  = BudgetItem.create! valid_attributes
