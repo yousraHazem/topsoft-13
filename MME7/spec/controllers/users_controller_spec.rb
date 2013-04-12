f7cb6e6753d7fa2e684ba12e353f50082c31def8
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-12 17:44:52 +0200

    fixed code

--- MME7/spec/controllers/users_controller_spec.rb
+++ MME7/spec/controllers/users_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
+#Authored by:Toka Abdelgabar 22-1925
 describe UsersController do
 def valid_attributes
       {:project_id => '1', :user_id => '1',}
-  end
+end
+#Authored by:Toka Abdelgabar 22-1925
   describe "GET get_myprojects" do
     it "assigns all projectusers as @projectusers" do
       projectusers = ProjectUser.create! valid_attributes
