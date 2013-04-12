7cfa782bb6d9fba926e70068c34ac6c409c75153
Riham Gamal <riham.gamal93@gmail.com>
2013-04-12 18:23:51 +0200

    203 corrected the doc

--- MME7/spec/controllers/projects_controller_spec.rb
+++ MME7/spec/controllers/projects_controller_spec.rb
@@ -13,6 +13,7 @@ describe ProjectsController do
     {:name => 'proj', :start_date => "7/7/2013" , :end_date => "7/8/2014", :description => "blablablablabla"}
   end
 
+   #Riham Gamal Hamed 22-3871
    describe "GET show" do
     it "assigns the requested project as @project" do
       project = Project.create! valid_attributes
