2ab04a5e5f837f325fcf4325f9ce38c3d7b2b927
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-12 17:28:15 +0200

    fixed code

--- MME7/app/controllers/users_controller.rb
+++ MME7/app/controllers/users_controller.rb
@@ -2,9 +2,8 @@
 	#Authored by Toka Omar  id:22-1925
   	#this method has no inputs and outputs: the projects of the current user  
   	#the method returs a list of projects the user has  
-def get_myprojects
-   @projectusers = ProjectUser.find(:all, 
-   :conditions => {:user_id => 1}) #current_user
+    def get_myprojects
+   @projectusers = ProjectUser.find(:all,:conditions => {:user_id => 1}) #current_user
    #@user_name = User.find(1) #current_user
     end 
 end 
