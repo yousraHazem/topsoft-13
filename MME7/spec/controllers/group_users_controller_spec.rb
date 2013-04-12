7bafaa18c19fc55f56c4280559d6b42a5170d1f4
mariam <mariamismael@live.com>
2013-04-12 13:20:12 +0200

    adding testing to create post

--- MME7/spec/controllers/group_users_controller_spec.rb
+++ MME7/spec/controllers/group_users_controller_spec.rb
@@ -3,7 +3,7 @@ describe GroupUsersController do
   describe "Post Join" do
     describe "with valid params" do
       it "creates a new groupuser" do       
-        response.should redirect_to(:controller => 'groups', :action => 'show')
+        response.should redirect_to(:controller => 'groups',:action => "show")
       end
     end
   end
