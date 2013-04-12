386bb5287f5fa8f6c93719fba470edf1e1cf68e0
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-12 12:23:53 +0200

    #150

--- MME7/app/models/project_user.rb
+++ MME7/app/models/project_user.rb
@@ -2,5 +2,7 @@
   attr_accessible :project_id , :user_id , :is_creator
   belongs_to :project
   belongs_to :user
-
+ def isProjectCreator(current_user, project_id)
+  ProjectUser.where(:user_id => current_user, :project_id => project_id, :is_creator => true).exists?  
+ end 
 end
