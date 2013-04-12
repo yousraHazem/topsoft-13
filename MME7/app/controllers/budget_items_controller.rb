b6580b1e4d5e7feaffe68859f0448d1a1e50b8fb
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-12 17:38:53 +0200

    fixed code

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -1,8 +1,8 @@
 ﻿class BudgetItemsController < ApplicationController
-#Authored by Toka Omar  id:22-1925
-#this method has no inputs and outputs: the operational budgetitems of 
-#the current project being viewd 
-#the method returs a list of operational budgetitems a guest could see  
+    #Authored by Toka Omar  id:22-1925
+    #this method has no inputs and outputs: the operational budgetitems of 
+    #the current project being viewd 
+    #the method returs a list of operational budgetitems a guest could see  
     def viewoperationalBI
     	@budget_items = BudgetItem.find(:all,:conditions => {:operational =>true,:project_id => params[:id]})
   		#@project_name = Project.find(params[:id]) #@id
