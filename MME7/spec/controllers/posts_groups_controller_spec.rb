a8508cfef05e0608937bd663cf7c2e6af2f667e7
mariam <mariamismael@live.com>
2013-04-12 12:05:39 +0200

    tests

--- MME7/spec/controllers/posts_groups_controller_spec.rb
+++ MME7/spec/controllers/posts_groups_controller_spec.rb
@@ -45,43 +45,47 @@ describe "POST create" do
     end
   end
 
-    describe "PUT updateProject" do
+  describe "PUT update" do
     describe "with valid params" do
-      it "updates the requested project" do
+      it "updates the requested post" do
         post = Post.create! valid_attributes
-        put :updatePostGroup, {:id => post.to_param, :post => update_attributes }
+        # Assuming there are no other <%= table_name %> in the database, this
+        # specifies that the <%= class_name %> created on the previous line
+        # receives the :update_attributes message with whatever params are
+        # submitted in the request.
+        put :updatePostGroup, {:id => post.to_param, :post => valid_attributes}
       end
 
-      it "assigns the requested project as @project" do
+      it "assigns the requested post as @post" do
         post = Post.create! valid_attributes
         put :updatePostGroup, {:id => post.to_param, :post => valid_attributes}
         assigns(:post).should eq(post)
       end
 
-
-      it "redirects to the project" do
+      it "redirects to the show group" do
         post = Post.create! valid_attributes
-        put :updatePostGroup, {:id => post.to_param, :@post => valid_attributes}
-        response.should redirect_to("http://test.host/groups/show/1")
+        put :updatePost, {:id => post.to_param, :post => valid_attributes}
+        
+        response.should redirect_to(:controller => 'groups',:action => "show")
       end
     end
 
     describe "with invalid params" do
-      it "assigns the project as @project" do
-        project = Post.create! valid_attributes
+      it "assigns the post as @post" do
+        post = Post.create! valid_attributes
+        # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
-        put :updatePostGroup, {:id => post.to_param, :post => invalid_attributes}
+        put :updatePostGroup, {:id => post.to_param, :post => valid_attributes}
         assigns(:post).should eq(post)
       end
 
-      it "re-renders the 'editProject' template" do
+      it "re-renders the group show template" do
         post = Post.create! valid_attributes
+        # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
-        put :updatePostGroup, {:id => post.to_param, :post => invalid_attributes}
-        response.should render_template("editProject")
+        put :updatePostGroup, {:id =>post.to_param, :post => invalid_attributes}
+        response.should redirect_to(:controller => 'groups',:action => "show")
       end
     end
-  
-  
-end
+  end
 end
