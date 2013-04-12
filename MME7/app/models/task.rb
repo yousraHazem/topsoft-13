6ada7a1da5b14dad621e90cf2b5353dcc2280471
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-12 02:47:00 +0200

    #183

--- MME7/app/models/task.rb
+++ MME7/app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ActiveRecord::Base
-  attr_accessible :description , :project_id , :assigned , :title
-has_one :budget_item 
+  attr_accessible :description , :project_id , :title , :assigned
+  has_one :budget_item 
   has_many :users 
   belongs_to :project 
+  has_many :task_users
+  has_many :users , :through => :task_users
 end
