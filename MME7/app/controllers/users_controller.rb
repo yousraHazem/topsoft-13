aba54f170b2de163153484554518c2262ad9f80f
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-12 23:30:09 +0200

    merging my branches together

--- MME7/app/controllers/users_controller.rb
+++ MME7/app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 ﻿class UsersController < ApplicationController
+ #Authored by Toka Omar  id:22-1925
+ #this method has no inputs and outputs: the projects of the current user  
+ #the method returs a list of projects the user has  
+ def get_myprojects
+ 	@projectusers = ProjectUser.find(:all,:conditions => {:user_id => 1})
+ end
 end 
