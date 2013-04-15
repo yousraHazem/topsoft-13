d8f4cd789a2ca3107b4d0d6f96ba7f28324d1ae3
mariam <mariamismael@live.com>
2013-04-15 14:01:43 +0200

    tests #114 #115

--- MME7/app/models/comment.rb
+++ MME7/app/models/comment.rb
@@ -1,17 +1,6 @@
 class Comment < ActiveRecord::Base
-
-<<<<<<< HEAD
-
     attr_accessible :post_id , :user_id , :comment 
 	belongs_to :user #comments creator
-=======
-    attr_accessible :post_id , :user_id , :comment 
-
-
-	belongs_to :user 
->>>>>>> ee79f9796525e283f8d46ea67f9a1461f9865a94
 	belongs_to :post
 	validates_presence_of :comment
-
- 
 end
