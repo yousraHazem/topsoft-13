bcfd0bab93b7fa149073f806c451b1eccf9e4f6e
mariam <mariamismael@live.com>
2013-04-15 14:28:22 +0200

    only adding the tests of communities #114 #115

--- MME7/spec/helpers/group_user_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe GroupUsersHelper do
-  describe "is this is already a member" do
-    it "returns true" do
-      helper.ifMember.should be_true
-    end
-  end
-end
