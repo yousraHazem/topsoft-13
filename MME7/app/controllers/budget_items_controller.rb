88bac1c7658ccd8a982efa56fd713c0f7b4b10bf
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-15 14:30:22 +0200

    view all budget items done and tested

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -1,3 +1,10 @@
 class BudgetItemsController < ApplicationController
+  #Authored by Toka Omar  id:22-1925
+  #Inputs:project_id and Outputs:all  budgetitems 
+  #Returs: Array of Budget Items
+  def viewAllBI 
+    	@budget_items = BudgetItem.find(:all,:conditions => {:project_id => 1})
+  end
+
 end
 
---
+++ MME7/spec/controllers/budget_items_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe BudgetItemsController do
+
+  def valid_attributes
+      {:name => 'comOne',:operational => 'true',:project_id =>'1'}
+  end
+  #Authored by:Toka Abdelgabar 22-1925
+  describe "GET viewAllBI", scope: true do
+    it "assigns all budget_items as @budget_items" do
+      budget_item  = BudgetItem.create! valid_attributes
+      get :viewAllBI, {:project_id => 1}  
+     assigns(:budget_items).should eq([budget_item])
+   end 
+ end 
+end
