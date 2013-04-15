04ff273f2763a3ba99f98f0f9357e162668b78f7
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-15 18:11:36 +0200

    removed my branch from master

--- MME7/app/controllers/budget_sources_controller.rb
+++ MME7/app/controllers/budget_sources_controller.rb
@@ -1,13 +1,4 @@
 
 class BudgetSourcesController < ApplicationController
 
-def list
-
-	@sources = BudgetSource.search(params[:search])
-end
-
-def show
-	@source = BudgetSource.find(params[:source_id])
-end
-
 end
