67f44add091c99af41a0f09a6b50964df3f38baf
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-16 21:43:27 +0200

    added notification jquery file

---
+++ MME7/app/controllers/nots_controller.rb
@@ -0,0 +1,14 @@
+﻿class NotsController < ApplicationController
+
+
+
+
+ def index
+
+ 	 respond_to do |format|
+        format.html {redirect_to :controller=>'subjects',:action=>'list'}
+        format.js
+        end
+ 	end	
+ 	
+end
