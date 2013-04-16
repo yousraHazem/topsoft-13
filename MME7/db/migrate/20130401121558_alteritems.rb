d784c8456dcd664e7c54e0e0e4b94eec2d6d6695
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-16 02:44:26 +0200

    ammedned tables #223

--- MME7/db/migrate/20130401121558_alteritems.rb
+++
@@ -1,8 +0,0 @@
-class Alteritems < ActiveRecord::Migration
-  def up
-  	add_column :budget_items , :project_id, :integer
-  end
-
-  def down
-  end
-end
--- MME7/db/schema.rb
+++ MME7/db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -16,13 +15,13 @@ ActiveRecord::Schema.define(:version => 20130412003037) do
 
   create_table "budget_components", :force => true do |t|
     t.string   "name"
-    t.integer  "total_quantity",     :default => 0
+    t.integer  "total_quantity"
     t.string   "status",             :default => "Pending"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.integer  "budget_item_id"
-    t.integer  "quantity_purchased", :default => 0
     t.integer  "spent"
+    t.integer  "quantity_purchased", :default => 0
     t.integer  "unit_price"
     t.integer  "total"
   end
@@ -33,11 +32,9 @@ ActiveRecord::Schema.define(:version => 20130412003037) do
     t.boolean  "operational"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-
-    t.integer  "total"
     t.integer  "spent"
     t.integer  "project_id"
-
+    t.integer  "total"
   end
 
   create_table "budget_items_users", :id => false, :force => true do |t|
@@ -48,9 +45,9 @@ ActiveRecord::Schema.define(:version => 20130412003037) do
   create_table "budget_source_projects", :force => true do |t|
     t.integer  "budget_source_id"
     t.integer  "project_id"
-    t.integer  "amount"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.integer  "amount"
   end
 
   create_table "budget_sources", :force => true do |t|
@@ -60,23 +57,6 @@ ActiveRecord::Schema.define(:version => 20130412003037) do
     t.datetime "updated_at", :null => false
   end
 
-
-
-  create_table "budget_sources_projects", :id => false, :force => true do |t|
-    t.integer "budget_source_id"
-    t.integer "project_id"
-  end
-create_table "budgets", :force => true do |t|
-    t.integer  "project_id"
-    t.integer  "amount"
-    t.integer  "raised"
-    t.integer  "noot_raised"
-    t.integer  "spent"
-    t.integer  "not_spent"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
     t.integer  "user_id"
@@ -84,8 +64,6 @@ create_table "budgets", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
   create_table "communities", :force => true do |t|
     t.string   "title"
@@ -118,13 +96,6 @@ create_table "budgets", :force => true do |t|
     t.integer  "community_id"
   end
 
-  create_table "groups_users", :id => false, :force => true do |t|
-    t.integer "group_id"
-    t.integer "user_id"
-  end
-
-  add_index "groups_users", ["group_id", "user_id"], :name => "index_groups_users_on_group_id_and_user_id"
-
   create_table "posts", :force => true do |t|
     t.text     "content"
     t.integer  "group_id"
@@ -147,14 +118,9 @@ create_table "budgets", :force => true do |t|
     t.date     "start_date"
     t.date     "end_date"
     t.text     "description"
-    t.boolean  "private_or_public"
-    t.boolean  "admin_or_member"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-    t.string   "title"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
-
-
 
   create_table "receipts", :force => true do |t|
     t.string   "name"
@@ -176,8 +142,8 @@ create_table "budgets", :force => true do |t|
     t.integer  "project_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
-    t.string   "title"
     t.boolean  "assigned",    :default => false
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
