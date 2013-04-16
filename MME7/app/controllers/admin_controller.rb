6bd1c1b4ad7ab3f66d9e3aab28a436c6615fbaf1
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-16 19:47:29 +0200

    removed donia's branch

--- MME7/app/controllers/admin_controller.rb
+++ MME7/app/controllers/admin_controller.rb
@@ -1,11 +1,3 @@
 ﻿class AdminController < ApplicationController
-	layout "admin_master"
- def show 
-  @admin = User.find(params[:id])
- end 
-  #Author: Donia Amer Shaarawy 22-0270
-  #it takes the id and it shows the user 
-	def new
-		@admin = User.new
-  end
+
 end
--- MME7/app/controllers/application_controller.rb
+++ MME7/app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 ﻿class ApplicationController < ActionController::Base
   protect_from_forgery
-  #Author: Donia Amer Shaarawy 22-0270
-  #it takes userid in a session and save it in a variable current_user
-  def current_user
-   return @current_user = User.find(session[:user_id])  if session[:user_id]
-  end
-  helper_method :current_user
-  include SessionsHelper
-  #Author: Donia Amer Shaarawy 22-0270
-  # Force signout to prevent CSRF (one-click attack or session riding) attacks
-  def handle_unverified_request
-    log_out
-    super
-  end
+
 end
