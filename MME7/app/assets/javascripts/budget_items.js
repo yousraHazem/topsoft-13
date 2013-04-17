f8904da9555c48475a4bee17fdecc8e7049ba48d
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-17 17:07:57 +0200

    removed bootstrap

--- MME7/app/assets/javascripts/budget_items.js
+++ MME7/app/assets/javascripts/budget_items.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-$('th').css("background-color","#ff0");
- $("tr:even").css("background-color","#cef");
- $('tr:odd').css("background-color","#ffc");
-});
-
-
 $(document).ready(function() {
 $("tr").mouseenter(function() {
 $(this).css("color","red");
