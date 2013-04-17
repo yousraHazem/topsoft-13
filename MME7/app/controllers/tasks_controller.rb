a2a2d2441812f9ffd44e22dd4755fd376ba98610
Riham Gamal <riham.gamal93@gmail.com>
2013-04-17 19:02:04 +0200

    removed comments controller

--- MME7/app/controllers/tasks_controller.rb
+++ MME7/app/controllers/tasks_controller.rb
@@ -1,8 +1,2 @@
 ﻿class TasksController < ApplicationController
-
-    # Author : Nayera Mohamed 22-3789 , this method shows the tasks of a certain project
-	def listTasks
-		@tasks = Task.where(:project_id=>params[:id])
-	end
-
 end
