6bf976b2f3065947b43874bef5ea5907d7c82537
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-17 18:32:50 +0200

    updated documentation and UMLs

--- MME7/app/controllers/budget_components_controller.rb
+++ MME7/app/controllers/budget_components_controller.rb
@@ -7,8 +7,10 @@
 	end 
 
 	def list
-		#authorized by: sarah ahmed id=22-1278
-		# calculates progress bar percentage and returns all the budget components of a certain budget item 
+		#authorized by: sarah ahmed id=22-1278 
+		#Description: calculates progress bar percentage and gets all the budget components of a certain budget item 
+		#returns: a list of budget components
+		#Arguments: none
 		@total = BudgetComponent.sum(:total, :conditions=>{:budget_item_id=>params[:id]})
 		@spent = BudgetComponent.sum(:spent, :conditions=>{:budget_item_id=>params[:id]})
 		@item = params[:id]
@@ -28,7 +30,9 @@
 
 	def create
 		#authorized by: sarah ahmed id=22-1278
-		# creates a budget component and then updates the spent and total attributes of its budget item
+		#Description: instantiate a new budget component and then updates the spent and total attributes of its budget item
+		#returns: none
+		#Arguments: none
 		@project_id = params[:project_id]
 	    @item = params[:id]
 	    @budget_item= BudgetItem.find(@item)
@@ -49,7 +53,9 @@
 
 	def edit
 		#authorized by: sarah ahmed id=22-1278
-		#gets the budget component of the passed id and view its details in the edit form
+		#Description: gets the budget component of the passed id and view its details in the edit form
+		#returns: none
+		#Arguments: none
 		@item = params[:item_id]
 		@project_id = params[:project_id]
 		@component = BudgetComponent.find(params[:id])
@@ -58,7 +64,9 @@
 
 	def update
 		#authorized by: sarah ahmed id=22-1278
-		#updates the budget component with the new submotted values and then updates the spent and total attributes of the associated budget
+		#Description: updates the budget component with the new submitted values and then updates the spent and total attributes of the associated budget
+		#returns: none
+		#Arguments: none
 		@item = params[:item_id]
 		@project_id = params[:project_id]
 		@budget_item= BudgetItem.find(@item)
@@ -79,7 +87,9 @@
 
 	def destroy
 		#authorized by: sarah ahmed id=22-1278
-		 #deletes the component with the passed id and then updates the spent and total attributes of the associated budget
+		# Description :deletes the component with the passed id and then updates the spent and total attributes of the associated budget
+	    #Arguments: none
+	    #returns: none
 	    @item = params[:item_id]
        
 	    BudgetComponent.find(params[:id]).destroy
