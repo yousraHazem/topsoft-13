0df6198b9fa04442999a10199e6a23b81dfad27e
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-17 14:08:05 +0200

    added the documentation and indentation to the code  #151

--- MME7/app/controllers/budget_sources_controller.rb
+++ MME7/app/controllers/budget_sources_controller.rb
@@ -1,15 +1,16 @@
 ﻿class BudgetSourcesController < ApplicationController
 
+
 def list 
 		@budget_sources = BudgetSource.all
     
 	end 
 
-
+#Author :yasmin Mahmoud This method creates a new instance of budget source and it has no arguments and return nothing 
 	def new
 		@budget_source = BudgetSource.new
 	end
-
+#Author :yasmin Mahmoud 22-1787 This method creates a  budget source with the parametres given from the user and it has no returns
 	def create 
 		@budget_source = BudgetSource.new(params[:budget_source])
 		if @budget_source.save
@@ -19,12 +20,12 @@ def list
 
 		end
 	end
-
+#Author :yasmin Mahmoud 22-1787 This method finds budget source with a given id and it has no arguments and return nothing 
 	def edit
     @budget_source = BudgetSource.find(params[:id])
-  end
-
-  def update
+  	end
+#Author :yasmin Mahmoud This method updates a budget source with the parametres given from the user and it has no returns
+  	def update
       @budget_source = BudgetSource.find(params[:id])
     if @budget_source.update_attributes(params[:budget_source])
       flash[:notice] = "Project successfully updated"
@@ -34,11 +35,12 @@ def list
         
     end
   end
-
-  def delete
+#Author :yasmin Mahmoud 22-1787 This method finds budget source with a given id and it has no arguments and return nothing 
+  	def delete
       @budget_source = BudgetSource.find(params[:id])
     end
-    
+    #Author :yasmin Mahmoud  22-1787 This method deletes a budget source it has no returns
+
     def destroy
       BudgetSource.find(params[:id]).destroy
       redirect_to(:action => 'list')
