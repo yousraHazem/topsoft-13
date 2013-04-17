5eacfd677f214f88d608575a55b8d1b076422d85
mariam <mariamismael@live.com>
2013-04-17 14:09:53 +0200

    adding tests #175

--- MME7/spec/helpers/group_user_helper_spec.rb
+++ MME7/spec/helpers/group_user_helper_spec.rb
@@ -1,15 +1,14 @@
 
 require 'spec_helper'
-
-describe GroupUser do
+describe GroupUsersHelper do
 it "should return true if he's the creator " do
-user = User.new(:name => "name 1", :email => "name@hotmail" , :phone_Nr =>"123456" , :address => "aaaaa" , :username => "yasmin1" , :isAdmin => false, :id => 1)
+user = User.new(:name => "name 1", :email => "name@hotmail" , :phone_Nr =>"123456" , :address => "aaaaa" , :username => "yasmin1" , :isAdmin => false)
 user.save
 userid= user.id
-group = Group.new(:group_name => "project1" , :id => 1, :description=> "jnhb" , :levels => "3")
+group = Group.new(:group_name => "project1" ,:description => "blablablablabla", )
 group.save
 groupid = group.id
-member = GroupUser.new(:user_id => 1, :group_id =>1 )
+member = GroupUser.new( :group_id =>groupid , :user_id => userid, :is_creator => false )
 member.save
 member.ifMember(userid , groupid).should be_true
 end
