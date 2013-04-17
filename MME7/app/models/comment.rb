deb5ef833dca3323018df4189f41c2e389b3559e
nayera-muhammad <nayera.muhammad@gmail.com>
2013-04-17 21:48:05 +0200

    adjusted code #189

--- MME7/app/models/comment.rb
+++ MME7/app/models/comment.rb
@@ -1,16 +1,14 @@
 ﻿class Comment < ActiveRecord::Base
 
     attr_accessible :post_id , :user_id , :comment 
-    
+    belongs_to :user 
+	belongs_to :post
+ 
     #Author: Nayera Mohamed 22-3789 
     # Args : current user and comment id
     # returns : boolean
 	def isCommentCreator(current_user , comment_id)
         Comment.where(:user_id => current_user, :id => comment_id).exists?  
     end 
-
-
-	belongs_to :user 
-	belongs_to :post
- 
+	
 end
--- MME7/app/models/project_user.rb
+++ MME7/app/models/project_user.rb
@@ -1,14 +1,12 @@
 ﻿class ProjectUser < ActiveRecord::Base
   attr_accessible :project_id , :user_id , :is_creator
- 
-
+  belongs_to :project
+  belongs_to :user
     #Author: Nayera Mohamed 22-3789 
     # Args : project id
     # returns : an array of memebers in a project
 	def get_projectmembers(project_id)
  	     @projectmembersid = ProjectUser.find(:all, :conditions => {:project_id => project_id })
     end
-
-  belongs_to :project
-  belongs_to :user
+ 
 end
