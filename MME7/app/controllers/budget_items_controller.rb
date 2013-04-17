370f93fb24bc444d738b6f2ee3714fa1901d569b
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-17 19:58:05 +0200

    added my documentation and code #150

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -7,13 +7,13 @@
 		
 		#@raised = BudgetSourceProject.sum(:amount , :conditions =>{:project_id => params[:id]})
 	end 
-# Author :Yasmin Mahmoud 22-1787 , Method new takes the id of the project and generates a new instanse of the budgetitem
+# Author :Yasmin Mahmoud 22-1787 , Method new takes the id of the project and generates a new instanse of the budgetitem no returns or arguments 
 	def new 
 		@project = params[:id]
 		@budget_item  = BudgetItem.new
 	end
 
-# Author :Yasmin Mahmoud 22-1787 , Method create takes attributes from the new form and enters it in the table 
+# Author :Yasmin Mahmoud 22-1787 , Method create takes attributes from the new form and enters it in the table no returns or arguments 
 	def create 
 		@project = params[:id]
 		@budget_item  = BudgetItem.new(params[:budget_item])
@@ -24,14 +24,14 @@
 		end
 	end
 
-	# Author :Yasmin Mahmoud 22-1787 , Method edit finds the budgetitem with the id taken from the params 
+	# Author :Yasmin Mahmoud 22-1787 , Method edit finds the budgetitem with the id taken from the params no returns or arguments 
 
 	def edit
 		@item = params[:item]
 		@project = params[:id]  
 		@budget_item  = BudgetItem.find(params[:item])
 	end
- # Author :Yasmin Mahmoud 22-1787 , Method update takes attributes from the edit form and updates the table 
+ # Author :Yasmin Mahmoud 22-1787 , Method update takes attributes from the edit form and updates the table no returns or arguments 
 	def update
 		@item = params[:item]
 		@project = params[:id]
