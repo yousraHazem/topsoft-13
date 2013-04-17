644601b463b31e4e80b45ad488d38dd260d1525e
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-17 12:17:54 +0200

    removed master controller #152

--- MME7/app/controllers/budget_sources_controller.rb
+++ MME7/app/controllers/budget_sources_controller.rb
@@ -1,13 +1,5 @@
 
 class BudgetSourcesController < ApplicationController
 
-def list
-
-	@sources = BudgetSource.search(params[:search])
-end
-
-def show
-	@source = BudgetSource.find(params[:source_id])
-end
 
 end
--- MME7/app/controllers/sessions_controller.rb
+++ MME7/app/controllers/sessions_controller.rb
@@ -1,29 +1,5 @@
 ﻿class SessionsController < ApplicationController
-  #Author: Donia Amer Shaarwy
-  #takes in the username and password to do the login method 
-  def new
-  end
-  def create
-    @user = User.find_by_username(params[:username])
-    if @user && @user.authenticate(params[:password])
-     session[:user_id] = @user.id
-     flash[:success] = "مرحب بيك فى ؤمن االماء حيه"
-      if @user.isAdmin?
-       redirect_to(:controller=>'admin',:action=>'show' ,:id=> @user.id)
-      else
-       redirect_to @user, :notice => "تسجيل الدخول!"
-      end 
-    else
-     flash[:error] = 'غير صالحة اسم المستخدم / كلمة السر'
-     render 'new'
-    end
-  end
- #Author: Donia Amer Shaarwy
- #takes in the session id and trunes it in nil and redirect to the home page 
- def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "تسجيل الخروج!"
-  end
+ 
 end
 
 
