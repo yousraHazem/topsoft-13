219ef1f909012c85b8334346a631069326d7e5c7
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-18 21:22:16 +0200

    removed spaces

--- MME7/app/helpers/project_users_helper.rb
+++ MME7/app/helpers/project_users_helper.rb
@@ -1,7 +1,4 @@
 module ProjectUsersHelper
-
-    
-
     # Author :Yasmin Mahmoud 22-1787 This method checks if the current user is a creator of the project he is trying to access 
   def isCreator(current_user, project_id)
   		ProjectUser.where(:user_id => current_user, :project_id => project_id, :is_creator => true).exists?  
