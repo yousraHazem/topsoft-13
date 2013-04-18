aae62148c8f04c4e500144626b65fd3c1a11fa1a
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-18 16:04:02 +0200

    fixed senario issue #184

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -5,7 +5,6 @@ class BudgetItemsController < ApplicationController
   def viewAllBI 
       @project = Project.find(params[:id])
     	@budget_items = BudgetItem.find(:all,:conditions => {:project_id => params[:id]})
-      @projectuser = ProjectUser.find(:all,:conditions => {:project_id => params[:id],:user_id => 5})  #change to current user 
   end
 end
 
