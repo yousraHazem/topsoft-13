e639ec6e71232042feff3f12a1ce45b4f14db600
mariam <mariamismael@live.com>
2013-04-19 16:56:45 +0200

    fixing migrations #114 #115

--- MME7/app/models/community.rb
+++ MME7/app/models/community.rb
@@ -1,13 +1,9 @@
+
 class Community < ActiveRecord::Base
-
    attr_accessible :title , :social_profile_info , :contact_info ,:is_dismissed
-
    has_many :groups, :dependent => :destroy
    has_and_belongs_to_many :projects
-   validates_presence_of :title
-
-   validates_uniqueness_of :title
-   validates_presence_of :social_profile_info
-
-
+   validates_presence_of :title , :message => "يجب أن تدخل اسم "
+   validates_uniqueness_of :title, :message => "عفوا هذا الاسم قد استخدم من قبل "
+   validates_presence_of :social_profile_info, :message => "يجب أن تدخل وصف "
 end
