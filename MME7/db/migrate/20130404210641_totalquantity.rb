cab61e848169f799607c5b8d35f6139dfb881e01
tokaabdelgabar <toka.abdelgabar@gmail.com>
2013-04-19 17:19:32 +0200

    fixing the migrations

--- MME7/db/migrate/20130404210641_totalquantity.rb
+++
@@ -1,8 +0,0 @@
-class Totalquantity < ActiveRecord::Migration
-  def up
-  	change_column :budget_components , :total_quantity ,:integer , :default=> 0
-  end
-
-  def down
-  end
-end
--- MME7/db/schema.rb
+++ MME7/db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130418140510) do
-
+ActiveRecord::Schema.define(:version => 20130419113340) do
 
   create_table "budget_components", :force => true do |t|
     t.string   "name"
-    t.integer  "total_quantity"
+    t.integer  "total_quantity",     :default => 0
     t.string   "status",             :default => "Pending"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
@@ -26,14 +24,11 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.integer  "quantity_purchased", :default => 0
     t.integer  "unit_price"
     t.integer  "total"
-
   end
 
   create_table "budget_items", :force => true do |t|
     t.integer  "task_id"
-    t.integer  "budget_id"
     t.string   "name"
-    t.integer  "status"
     t.boolean  "operational"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
@@ -50,10 +45,9 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
   create_table "budget_source_projects", :force => true do |t|
     t.integer  "budget_source_id"
     t.integer  "project_id"
-    t.integer  "amount",           :default => 0
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
-
+    t.integer  "amount",           :default => 0
   end
 
   create_table "budget_sources", :force => true do |t|
@@ -61,6 +55,22 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.integer  "amount"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "budget_sources_projects", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "budget_source_id"
+  end
+
+  create_table "budgets", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "amount"
+    t.integer  "raised"
+    t.integer  "noot_raised"
+    t.integer  "spent"
+    t.integer  "not_spent"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "comments", :force => true do |t|
@@ -71,14 +81,13 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.datetime "updated_at", :null => false
   end
 
-  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
-
   create_table "communities", :force => true do |t|
     t.string   "title"
     t.text     "social_profile_info"
     t.text     "contact_info"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.boolean  "is_dismissed"
   end
 
   create_table "communities_projects", :id => false, :force => true do |t|
@@ -103,13 +112,21 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.integer  "community_id"
   end
 
+  create_table "groups_users", :id => false, :force => true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
+
+  add_index "groups_users", ["group_id", "user_id"], :name => "index_groups_users_on_group_id_and_user_id"
+
   create_table "posts", :force => true do |t|
     t.text     "content"
     t.integer  "group_id"
     t.integer  "project_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.integer  "user_id"
+    t.boolean  "is_group",   :default => false
   end
 
   create_table "project_users", :force => true do |t|
@@ -125,11 +142,14 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.date     "start_date"
     t.date     "end_date"
     t.text     "description"
-
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
     t.boolean  "is_frozen",   :default => false
+  end
 
+  create_table "projects_users", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
   end
 
   create_table "receipts", :force => true do |t|
@@ -140,13 +160,20 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.string   "image"
   end
 
+  create_table "task_users", :force => true do |t|
+    t.integer  "task_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "tasks", :force => true do |t|
     t.text     "description"
     t.integer  "project_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
-    t.string   "title"
     t.boolean  "assigned",    :default => false
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
@@ -155,14 +182,9 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.string   "phone_Nr"
     t.string   "address"
     t.string   "username"
-    t.boolean  "isAdmin",         :default => false
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.string   "password_digest"
-    t.string   "remember_token"
+    t.boolean  "isAdmin"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
