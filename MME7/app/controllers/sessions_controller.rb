4f69af1e490e92efb66677a2c64ed8c6341e93e7
Donia Amer Shaarawy <shaarawy.donia@gmail.com>
2013-04-20 14:45:03 +0200

    facebook config #226

--- MME7/app/controllers/sessions_controller.rb
+++ MME7/app/controllers/sessions_controller.rb
@@ -26,6 +26,9 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, :notice => "logged out"
   end
+  #Donia Amer Shaarawy 22-0270
+  #this creates the page for the user that login with his 
+  #facebook account it takes his facebook email and passowrd and returns to the user page 
  def createFacebook
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
--- MME7/config/initializers/omniauth.rb
+++ MME7/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '356609801110887', '92b14f77debbc7953c9e05047ac4ea7b', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :facebook, '356609801110887', '92b14f77debbc7953c9e05047ac4ea7b'
 end
