b893d40c5943975a6cb9254d568b7aa75b2089ed
mariam <mariamismael@live.com>
2013-04-21 01:19:23 +0200

    views and tests #175

---
+++ MME7/spec/controllers/group_user_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe GroupUsersController do
+  def valid_attributes
+   {:group_id => 1, :user_id => 1, :is_creator => false}
+  end
+
+describe "POST create" do
+    describe "with valid params" do
+      it "creates a new group user" do
+        expect {
+          post :join, {:groupuser => valid_attributes}
+        }.to change(GroupUser, :count).by(1)
+      end
+
+      it "assigns a newly created groupuser as @groupuser" do
+        post :join, {:groupuser => valid_attributes}
+        assigns(:newuser).should be_a(GroupUser)
+        assigns(:newuser).should be_persisted
+      end
+
+      it "redirects to the group show" do
+        post :join, {:groupuser => valid_attributes}
+        response.should redirect_to(:controller => 'groups',:action => "show")
+      end
+    end
+
+  end
+end
