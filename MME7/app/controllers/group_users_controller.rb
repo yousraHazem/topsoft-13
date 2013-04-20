775e65ba3365c42294e380ce28515f37402502ac
mariam <mariamismael@live.com>
2013-04-20 23:41:04 +0200

    added ifmember #175

--- MME7/app/controllers/group_users_controller.rb
+++ MME7/app/controllers/group_users_controller.rb
@@ -1,3 +1,14 @@
+#encoding: UTF-8
 class GroupUsersController < ApplicationController
-	end 
 
+#Author Mariam Ismail, 22-3456
+#This method adds the user to a group
+# it takes the user ID and returns nothing it only adds a record it the groupusers table
+  def join
+
+    @group_id = params[:id]
+    @user_id = current_user.id
+    @newuser= GroupUser.new(:group_id => @group_id, :user_id => @user_id)
+    @newuser.save
+    redirect_to(:controller => 'groups', :action => 'show', :id => @group_id)
+  end
--- MME7/app/helpers/group_users_helper.rb
+++ MME7/app/helpers/group_users_helper.rb
@@ -12,4 +12,12 @@ module GroupUsersHelper
  	m = GroupUser.new(:group_id => group_id, :user_id => user_id)
  	m.save
    end
+
+   # Author: Mariam Ismail 22-3456
+   # This method checks if a user is an already exisiting member in a specific group or not
+   # This method takes the group id and user id, and returns true/false
+   def ifMember(group_id,user_id)
+      GroupUser.where(:group_id => group_id, :user_id => user_id).exists? #change to current_user_id  
+   end
+
 end
