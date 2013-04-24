18f6ec5756d859daec3520ed7552ec085410e09a
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-24 02:21:18 +0200

    added column is group in comments #223

--- MME7/db/schema.rb
+++ MME7/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419113340) do
+ActiveRecord::Schema.define(:version => 20130424001513) do
 
   create_table "budget_components", :force => true do |t|
     t.string   "name"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(:version => 20130419113340) do
     t.integer  "post_id"
     t.integer  "user_id"
     t.text     "comment"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "is_group",   :default => false
   end
 
   create_table "communities", :force => true do |t|
