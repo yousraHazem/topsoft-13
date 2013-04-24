6deaad412fa1b31d1070d8355e2b872d4440c2b1
sama akram abdelrahman <samaakram92@gmail.com>
2013-04-24 03:22:32 +0200

    fixed redundant commetn methods for projects and group #161 #207 #208 #172 #173 #174

--- MME7/db/schema.rb
+++ MME7/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418140510) do
+ActiveRecord::Schema.define(:version => 20130424001513) do
 
   create_table "budget_components", :force => true do |t|
     t.string   "name"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.integer  "post_id"
     t.integer  "user_id"
     t.text     "comment"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "is_group",   :default => false
   end
 
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
@@ -101,9 +102,10 @@ ActiveRecord::Schema.define(:version => 20130418140510) do
     t.text     "content"
     t.integer  "group_id"
     t.integer  "project_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.integer  "user_id"
+    t.boolean  "is_group",   :default => false
   end
 
   create_table "project_users", :force => true do |t|
