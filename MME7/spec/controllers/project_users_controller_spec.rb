507949e013a53892fd2e61ba263be34a34b9ea0e
Riham Gamal <riham.gamal93@gmail.com>
2013-04-26 17:27:11 +0200

    solving rspec problem #273

--- MME7/spec/controllers/project_users_controller_spec.rb
+++ MME7/spec/controllers/project_users_controller_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 # Author : Nayera Mohamed 22-3789
 describe ProjectUsersController do
 
-it"should return list of members in a project" do
-	project = Project.new(:project_name => "proj1", :start_date  => "7/8/2013", :end_date => "7/8/2014" , :description => "blablablablablabla")
-	project.save
-	projectid = project.id
-	projectuser = ProjectUser.new(:project_id => 1 , :user_id => 1, :is_creator => false)
-	projectuser.save
-	projectuser = ProjectUser.new(:project_id => 1 , :user_id => 2, :is_creator => false)
-	projectuser.save
-	projectuser.getProjectmembers(projectid)
-end
-   
+describe "POST promoteMembers" do
+    it "should promote a member to be a creator" do
+      get :promoteMembers
+      @user = ProjectUser.create!
+      put :update, {:id => projectuser.to_param}
+      if @user.save
+			redirect_to(:controller => 'project_users', :action => 'index')
+		else
+			redirect_to(:controller => 'projects', :action => 'show')
+	  end		
+    end
+end   
 
 end
