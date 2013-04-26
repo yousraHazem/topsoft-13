91029bc479d15cb6770e349cf32490e0c6357448
yasmin eltahan <yasmin.eltahan@gmail.com>
2013-04-26 05:49:09 +0200

    added tables for notification #223

--- MME7/app/models/notification.rb
+++ MME7/app/models/notification.rb
@@ -1,3 +1,6 @@
-﻿class Notification < ActiveRecord::Base
-  # attr_accessible :title, :body
+class Notification < ActiveRecord::Base
+  attr_accessible :content , :url , :image
+
+  has_many :notification_users
+  has_many :users ,:through => :notification_users
 end
