b43ca1d461bc092db7faeed3820ceb055c58b66b
mariam <mariamismael@live.com>
2013-04-27 13:43:26 +0200

    db/schema

--- MME7/db/schema.rb
+++ MME7/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426233602) do
+ActiveRecord::Schema.define(:version => 20130426030203) do
 
   create_table "budget_components", :force => true do |t|
     t.string   "name"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(:version => 20130426233602) do
     t.text     "contact_info"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.boolean  "is_dismissed"
   end
 
   create_table "communities_projects", :id => false, :force => true do |t|
