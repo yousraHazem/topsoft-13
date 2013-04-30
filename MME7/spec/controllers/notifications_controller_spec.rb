be09a9869952284c5950c8dcab13d2a525372ddd
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-30 16:27:02 +0200

    tests are done #282

--- MME7/spec/controllers/notifications_controller_spec.rb
+++ MME7/spec/controllers/notifications_controller_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
+require 'application_helper'
 
 describe NotificationsController do
-   include Devise::TestHelpers
+   #include Devise::TestHelpers
   
 
   def valid_attributes
    { :notification_id=> 1  ,:user_id=> 1 , :read=> false}
    end
-	  # describe "GET index" do
-   #  login_user
-   #    it "assigns new notifications as @nots" do
-   #    nots = NotificationUser.create! valid_attributes
-   #    get :index, {:notifcation_id => 1 , :user_id=>1}
-   #    puts  :nots
-   #    assigns(:nots).should eq([nots])
 
-   #    end
-   #    end
+	   describe "GET index" do
+      it "assigns new notifications as @nots" do
+      ApplicationController.any_instance.stub(:current_user).and_return(User.new)
+      User.any_instance.stub(:id).and_return(1)
+      nots = NotificationUser.create! valid_attributes
+      get :index, {:user_id=> 1, :after =>Time.now + 2592000}
+      assigns(:nots).should eq([nots])
+
+      end
+      end
 
    
 
@@ -29,35 +31,27 @@ describe NotificationsController do
    end
  
 
-      # describe "GET list" do
-      #    it "assigns new notifications as @notifications" do
-      #       notifications = NotificationUser.new
-      #       NotificationUser.should_receive(:where).and_return(notifications)
-      #       get :list , {:id=>1}
-      #       assigns(:notifications).should eq([notifications])
-      #    end
-      # end
+
+   describe "GET all" do
+      it "assigns new notifications as @notifications" do
+      notifications = NotificationUser.create! valid_attributes
+      ApplicationController.any_instance.stub(:current_user).and_return(User.new)
+      User.any_instance.stub(:id).and_return(1)
+      get :all, {:id=> 1}
+      assigns(:notifications).should eq([notifications])
+   end
+   end
 
 
-      # describe "GET all" do
-      # it "assigns notifications as @notifications" do
-      # notifications = NotificationUser.new 
-      # notifications.notification_id = 1
-      # notifications.user_id= 1
-      # notifications.save
-      # get 'all', {:id=>1}
-      # assigns(:notifications).should eq([notifications])
-
-      # end
-      # end
-
-      # describe "GET read" do
-      # it "assigns notifications as @notifications" do
-      # notifications = NotificationUser.create! valid_attributes
-      # get :read, {:id=>1}
-      # assigns(:notifications).should eq([notifications])
-
-      # end
-      # end
+     describe "GET read" do
+      it "assigns new notifications as @notifications" do
+      notifications = NotificationUser.create! valid_attributes
+    ApplicationController.any_instance.stub(:current_user).and_return(User.new)
+      User.any_instance.stub(:id).and_return(1)
+      get :read, {:id=> 1}
+      assigns(:notifications).should eq([notifications])
+   end
+   end
 
 end
+
