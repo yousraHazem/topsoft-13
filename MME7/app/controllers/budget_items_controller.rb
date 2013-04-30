74849e97ba5c8deb36c5e28a7b6d53ad02c91e2a
sarah ahmed mohamed <sarahabulfadl@gmail.com>
2013-04-30 17:02:38 +0200

    changed project params in controller  #282

--- MME7/app/controllers/budget_items_controller.rb
+++ MME7/app/controllers/budget_items_controller.rb
@@ -15,7 +15,7 @@ class BudgetItemsController < ApplicationController
 		# @total_budget = BudgetItem.sum(:total , :conditions => {:project_id => params[:id]})
 		# @total_spent = BudgetItem.sum(:spent , :conditions => {:project_id => params[:id]})
 		#@project_name = Project.find(params[:id])
-		 @tasks = Task.find(:all,:conditions=>{:project_id=> 1, :assigned=>false})
+		@tasks = Task.find(:all,:conditions=>{:project_id=>params[:id], :assigned=>false})
 		#@raised = BudgetSourceProject.sum(:amount , :conditions =>{:project_id => params[:id]})
 	end 
 
