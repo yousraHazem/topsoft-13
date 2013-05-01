e5f43a51dfb0e0037704185a172e15113ca47b12
Donia Amer Shaarawy <shaarawy.donia@gmail.com>
2013-05-01 14:57:01 +0200

    add validation for the client side #155

--- MME7/app/controllers/sessions_controller.rb
+++ MME7/app/controllers/sessions_controller.rb
@@ -29,18 +29,6 @@ class SessionsController < ApplicationController
   #Donia Amer Shaarawy 22-0270
   #this creates the page for the user that login with his 
   #facebook account it takes his facebook email and passowrd and returns to the user page 
- def createFacebook
-   raise request."omniauth.auth".to_yaml
-    user = User.from_omniauth("omniauth.auth")
-    session[:user_id] = user.id
-    redirect_to @user, :notice => "logged in"
-  end
-#Donia Amer Shaarawy 22-0270 
-#sgin out of facebook and the site 
-  def destroyFacebook
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "logged out"
-  end
 end
 
 
